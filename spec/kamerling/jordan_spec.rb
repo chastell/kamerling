d80d8b29d11d5cf3489a445c600d4b8d8f8f66c2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-04-14 20:23:11 +0200

    refactor Jordan spec

--- spec/kamerling/jordan_spec.rb
+++ spec/kamerling/jordan_spec.rb
@@ -5,29 +5,33 @@ require_relative '../spec_helper'
 module Kamerling describe Jordan do
   let(:jordan) { Jordan.new }
 
+  def with_jordan_running
+    EM.run do
+      yield
+      EM.stop
+    end
+  end
+
   describe '.new' do
     it 'starts the server on the given host and port' do
-      EM.run do
+      with_jordan_running do
         TCPSocket.new(jordan.host, jordan.port).close
-        EM.stop
       end
     end
   end
 
   describe '#host' do
     it 'returns the server’s host (127.0.0.1 by default)' do
-      EM.run do
+      with_jordan_running do
         jordan.host.must_equal '127.0.0.1'
-        EM.stop
       end
     end
   end
 
   describe '#port' do
     it 'returns the server’s port (random available by default)' do
-      EM.run do
+      with_jordan_running do
         (1024..65535).must_include jordan.port
-        EM.stop
       end
     end
   end
