6a6f7c1cf912b9710352992e10b6ce64e3ee69f2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-03-21 16:52:07 +0100

    Kamerling spec: clarity > obnoxiousness

--- spec/kamerling/jordan_spec.rb
+++ spec/kamerling/jordan_spec.rb
@@ -4,7 +4,7 @@ module Kamerling describe Jordan do
   describe '.new' do
     it 'starts a server on the given host and port' do
       Jordan.new host: '0.0.0.0', port: 1981
-      TCPSocket.new('0.0.0.0', 1981).close
+      TCPSocket.open '0.0.0.0', 1981
     end
 
     it 'defaults to localhost' do
@@ -16,8 +16,8 @@ module Kamerling describe Jordan do
       assert (1024..65535).include? j1.port
       assert (1024..65535).include? j2.port
       refute j1.port == j2.port
-      TCPSocket.new(j1.host, j1.port).close
-      TCPSocket.new(j2.host, j2.port).close
+      TCPSocket.open j1.host, j1.port
+      TCPSocket.open j2.host, j2.port
     end
   end
 end end
