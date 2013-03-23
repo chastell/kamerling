072f6da460db9b740cb39e33e310c583669124cd
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-03-23 11:50:16 +0100

    spec_helper: minimal double support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,20 @@ require 'bundler/setup'
 require 'minitest/autorun'
 
 require 'kamerling'
+
+class Double < OpenStruct
+  def initialize opts
+    callables, values = opts.partition { |_, val| val.is_a? Proc }
+    super Hash[values]
+    callables.each do |name, callable|
+      define_singleton_method(name) { |*args| callable.call(*args) }
+    end
+  end
+
+  alias ==   equal?
+  alias eql? equal?
+end
+
+def double opts = {}
+  Double.new opts
+end
