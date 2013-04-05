8420d55bb0a929926706b938452677f84f4dc896
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-04-05 09:16:31 +0200

    Yngo#register: handle the passed Addrinfo

--- lib/kamerling/yngo.rb
+++ lib/kamerling/yngo.rb
@@ -1,7 +1,8 @@
 module Kamerling class Yngo
-  Registration = Struct.new :client, :project
+  Registration = Struct.new :addrinfo, :client, :project
 
-  def register(client: client, project: project, repos: repos)
-    repos[:registrations] << Registration.new(client, project)
+  def register(addrinfo: addrinfo, client: client, project: project,
+               repos: nil)
+    repos[:registrations] << Registration.new(addrinfo, client, project)
   end
 end end
--- spec/kamerling/yngo_spec.rb
+++ spec/kamerling/yngo_spec.rb
@@ -3,10 +3,12 @@ require_relative '../spec_helper'
 module Kamerling describe Yngo do
   describe '#register' do
     it 'registers that the given client can do the given project' do
-      repo = MiniTest::Mock.new.expect :<<, nil,
-        [Yngo::Registration.new(client = double, project = double)]
+      registration = Yngo::Registration.new addrinfo = double,
+        client = double, project = double
+      repo = MiniTest::Mock.new.expect :<<, nil, [registration]
       repos = { registrations: repo }
-      Yngo.new.register client: client, project: project, repos: repos
+      Yngo.new.register addrinfo: addrinfo, client: client, project: project,
+        repos: repos
       repo.verify
     end
   end
