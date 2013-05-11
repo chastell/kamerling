2acc60c3beb2772747e94996a3cb52c9f5e427c9
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-12 00:48:46 +0200

    Receiver spec: switch to Bogus

--- spec/kamerling/receiver_spec.rb
+++ spec/kamerling/receiver_spec.rb
@@ -3,11 +3,13 @@ require_relative '../spec_helper'
 module Kamerling describe Receiver do
   describe '#receive' do
     it 'processes the result for a given task' do
-      repo = MiniTest::Mock.new.expect :<<, nil,
-        [{ client: client = fake, data: data = fake, task: task = fake }]
-      Receiver.new.receive client: client, data: data, repos: { results: repo },
-        task: task
-      repo.verify
+      client = fake :client
+      repo   = fake :repo
+      task   = fake :task
+      Receiver.new.receive client: client, data: 'data',
+        repos: { results: repo }, task: task
+      repo.must_have_received :<<,
+        [{ client: client, data: 'data', task: task }]
     end
   end
 end end
