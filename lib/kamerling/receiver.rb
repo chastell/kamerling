2b7f7eed31e03a4ac97a1a2f6ef08ab5dda5776f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-12 00:26:42 +0200

    Receiver#receive, Registrar#register: normalise kwargs

--- lib/kamerling/receiver.rb
+++ lib/kamerling/receiver.rb
@@ -1,5 +1,5 @@
 module Kamerling class Receiver
-  def receive(client: client, data: data, repos: repos, task: task)
+  def receive(client: raise, data: raise, repos: nil, task: raise)
     repos[:results] << { client: client, data: data, task: task }
   end
 end end
--- lib/kamerling/registrar.rb
+++ lib/kamerling/registrar.rb
@@ -1,6 +1,5 @@
 module Kamerling class Registrar
-  def register(addrinfo: addrinfo, client: client, project: project,
-               repos: nil)
+  def register(addrinfo: raise, client: raise, project: raise, repos: nil)
     repos[:registrations] << { addrinfo: addrinfo, client: client, project: project }
   end
 end end
