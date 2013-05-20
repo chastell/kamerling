ea7eefdce115ab16bb0ea1cfe107a53ac2874099
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-20 09:06:33 +0200

    factor out Handler#parse

--- lib/kamerling/handler.rb
+++ lib/kamerling/handler.rb
@@ -2,7 +2,7 @@ module Kamerling class Handler
   UnknownInput = Class.new RuntimeError
 
   def handle input, addr, receiver: Receiver.new, registrar: Registrar.new
-    message = Messages.const_get(input[0..3]).new input
+    message = parse input
     case message.type
     when 'RGST'
       registrar.register addr: addr, client_uuid: message.client_uuid,
@@ -11,6 +11,12 @@ module Kamerling class Handler
       receiver.receive addr: addr, client_uuid: message.client_uuid,
         data: message.data, task_uuid: message.task_uuid
     end
+  end
+
+  private
+
+  def parse input
+    Messages.const_get(input[0..3]).new input
   rescue NameError
     raise UnknownInput, input
   end
