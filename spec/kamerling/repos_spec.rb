a82dc0dac8159fa978be4f6824723ba78bb022b1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-26 11:52:36 +0200

    Repos spec: move migrations spec

--- spec/kamerling/repos_spec.rb
+++ spec/kamerling/repos_spec.rb
@@ -16,22 +16,20 @@ module Kamerling describe Repos do
       Repos[Client, repo: {}][uuid].must_be_nil
       Repos[Client, repo: { uuid => client }][uuid].must_equal client
     end
-
-    it 'makes sure there’s an actual db underneath' do
-      uuid   = '16B client  UUID'
-      client = Client[addr: Addr['127.0.0.1', 1981], uuid: uuid]
-      Repos[Client][uuid].must_be_nil
-      Repos[Client] << client
-      Repos[Client][uuid].must_equal client
-    end
   end
 
   describe '.db=' do
-    it 'allows passing a Sequel db and auto-migrates it' do
+    it 'auto-migrates the passed db' do
       db = Sequel.sqlite
       db.tables.wont_include :schema_info
       Repos.db = db
       db.tables.must_include :schema_info
+    end
+
+    it 'makes sure clients can be stored and retrieved' do
+      client = Client[addr: Addr['127.0.0.1', 1981], uuid: '16B client  UUID']
+      Repos << client
+      Repos[Client]['16B client  UUID'].must_equal client
     end
   end
 end end
