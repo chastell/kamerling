7a41d476832093edce78e0f9ebd6e2f545686f2b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-26 19:45:05 +0200

    simplify Task spec

--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -5,17 +5,15 @@ module Kamerling describe Task do
     it 'backtranslates a project_uuid to project' do
       Repos << project = Project[name: 'project name', uuid: '16B project UUID']
       Task.from_h(input: 'input', project_uuid: '16B project UUID',
-        uuid: '16B task    UUID').must_equal Task[input: 'input',
-          project: project, uuid: '16B task    UUID']
+        uuid: '16B task    UUID').project.must_equal project
     end
   end
 
   describe '#to_h' do
     it 'represents project as its UUID' do
       project = Project[name: 'project name', uuid: '16B project UUID']
-      Task[input: 'task input', project: project, uuid: '16B task    UUID'].to_h
-        .must_equal({ input: 'task input', project_uuid: '16B project UUID',
-          uuid: '16B task    UUID' })
+      Task[input: 'task input', project: project, uuid: '16B task    UUID']
+        .to_h[:project_uuid].must_equal '16B project UUID'
     end
   end
 end end
