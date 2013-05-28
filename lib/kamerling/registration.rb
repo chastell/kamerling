1b30cccdb6e224699a190828e57b7cd4b59535ea
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-28 18:44:58 +0200

    add Registration#to_h

--- lib/kamerling/registration.rb
+++ lib/kamerling/registration.rb
@@ -5,5 +5,12 @@ module Kamerling
     def initialize addr: raise, client: raise, project: raise, uuid: random_uuid
       super addr, client, project, uuid
     end
+
+    def to_h
+      super.tap do |hash|
+        hash.merge! host: addr.host, port: addr.port
+        hash.delete :addr
+      end
+    end
   end
 end
--- spec/kamerling/registration_spec.rb
+++ spec/kamerling/registration_spec.rb
@@ -1,13 +1,22 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Registration do
-  describe '.new' do
-    fakes :addr, :client, :project
+  fakes :addr, :client, :project
 
+  describe '.new' do
     it 'gives the registration a random UUID' do
       r1 = Registration[addr: addr, client: client, project: project]
       r2 = Registration[addr: addr, client: client, project: project]
       r1.uuid.wont_equal r2.uuid
+    end
+  end
+
+  describe '#to_h' do
+    it 'represents addr as a host + port pair' do
+      addr = Addr['127.0.0.1', 1981]
+      hash = Registration[addr: addr, client: client, project: project].to_h
+      hash[:host].must_equal '127.0.0.1'
+      hash[:port].must_equal 1981
     end
   end
 end end
