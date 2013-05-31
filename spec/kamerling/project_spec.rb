f27657950d454725ce785a48e336ccf6680c120f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 16:57:17 +0200

    clean-up Project spec

--- spec/kamerling/project_spec.rb
+++ spec/kamerling/project_spec.rb
@@ -3,9 +3,8 @@ require_relative '../spec_helper'
 module Kamerling describe Project do
   describe '.from_h' do
     it 'deserialises a project from a hash' do
-      hash = { name: 'project name', uuid: UUID.from_bin('16B project UUID') }
-      Project.from_h(hash).must_equal Project[name: 'project name',
-        uuid: UUID.from_bin('16B project UUID')]
+      hash = { name: name = 'project name', uuid: uuid = UUID.new }
+      Project.from_h(hash).must_equal Project[name: name, uuid: uuid]
     end
   end
 end end
