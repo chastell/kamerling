c96a2b4f361ef90877dc114059c0d44015267c08
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:54:38 +0200

    simplify Repos spec even more

--- spec/kamerling/repos_spec.rb
+++ spec/kamerling/repos_spec.rb
@@ -2,7 +2,6 @@ require_relative '../spec_helper'
 
 module Kamerling describe Repos do
   before { Repos.db = Sequel.sqlite }
-  let(:addr) { Addr['127.0.0.1', 1981] }
 
   describe '.<<' do
     it 'shuffles the object into the right repo' do
@@ -21,10 +20,6 @@ module Kamerling describe Repos do
   end
 
   describe '.db=' do
-    let(:client)  { Client[addr: addr, uuid: UUID.new] }
-    let(:project) { Project[name: 'project name', uuid: UUID.new] }
-    let(:task)    { Task[input: 'input', project: project, uuid: UUID.new] }
-
     it 'auto-migrates the passed db' do
       db = Sequel.sqlite
       db.tables.wont_include :schema_info
@@ -32,34 +27,17 @@ module Kamerling describe Repos do
       db.tables.must_include :schema_info
     end
 
-    it 'makes sure clients can be stored and retrieved' do
-      Repos << client
-      Repos[Client][client.uuid].must_equal client
-    end
-
-    it 'makes sure projects can be stored and retrieved' do
-      Repos << project
-      Repos[Project][project.uuid].must_equal project
-    end
-
-    it 'makes sure registrations can be stored and retrieved' do
-      Repos << project
-      Repos << client
+    it 'makes sure objects can be stored and retrieved' do
+      addr = Addr['127.0.0.1', 1981]
+      Repos << client  = Client[addr: addr, uuid: UUID.new]
+      Repos << project = Project[name: 'project name', uuid: UUID.new]
+      Repos << task    = Task[input: 'input', project: project, uuid: UUID.new]
       Repos << reg = Registration[addr: addr, client: client, project: project]
-      Repos[Registration][reg.uuid].must_equal reg
-    end
-
-    it 'makes sure results can be stored and retrieved' do
-      Repos << project
-      Repos << client
-      Repos << task
       Repos << res = Result[addr: addr, client: client, data: 'da', task: task]
+      Repos[Client][client.uuid].must_equal client
+      Repos[Project][project.uuid].must_equal project
+      Repos[Registration][reg.uuid].must_equal reg
       Repos[Result][res.uuid].must_equal res
-    end
-
-    it 'makes sure tasks can be stored and retrieved' do
-      Repos << project
-      Repos << task
       Repos[Task][task.uuid].must_equal task
     end
   end
