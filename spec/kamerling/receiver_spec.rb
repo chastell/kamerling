6740a2131c70341546905eb79563181ad7b61893
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:29:00 +0200

    simplify Receiver spec

--- spec/kamerling/receiver_spec.rb
+++ spec/kamerling/receiver_spec.rb
@@ -3,15 +3,12 @@ require_relative '../spec_helper'
 module Kamerling describe Receiver do
   describe '#receive' do
     fakes :addr, :client, :repo, :task
-    let(:cuuid) { UUID.new }
-    let(:tuuid) { UUID.new }
-    let(:repos) { {
-      Client => { cuuid => client }, Result => repo, Task => { tuuid => task },
-    } }
 
     it 'processes the result for a given task' do
-      Receiver.new.receive addr: addr, client_uuid: cuuid, data: 'data',
-        repos: repos, task_uuid: tuuid
+      repos = { Client => { client.uuid => client }, Result => repo,
+        Task => { task.uuid => task } }
+      Receiver.new.receive addr: addr, client_uuid: client.uuid, data: 'data',
+        repos: repos, task_uuid: task.uuid
       repo.must_have_received :<<, [Result[addr: addr, client: client,
         data: 'data', task: task, uuid: anything]]
     end
