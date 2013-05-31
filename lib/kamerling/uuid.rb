15cb44a2622d57f1337edc38f5b6c60d78b89d44
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 15:14:25 +0200

    add UUID.from_bin

--- lib/kamerling/uuid.rb
+++ lib/kamerling/uuid.rb
@@ -1,4 +1,8 @@
 module Kamerling module UUID
+  def self.from_bin bin
+    bin.unpack('H8H4H4H4H12').join '-'
+  end
+
   def self.new
     SecureRandom.uuid
   end
--- spec/kamerling/uuid_spec.rb
+++ spec/kamerling/uuid_spec.rb
@@ -1,6 +1,13 @@
 require_relative '../spec_helper'
 
 module Kamerling describe UUID do
+  describe '.from_bin' do
+    it 'creates a normal UUID representation from binary data' do
+      UUID.from_bin('16B project UUID')
+        .must_equal '31364220-7072-6f6a-6563-742055554944'
+    end
+  end
+
   describe '.new' do
     it 'creates a new random UUID' do
       UUID.new.must_match(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
