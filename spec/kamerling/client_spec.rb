1ccbcb91b119d2ec4553ede2f74c30cd069ff694
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 17:16:38 +0200

    clean-up Client spec

--- spec/kamerling/client_spec.rb
+++ spec/kamerling/client_spec.rb
@@ -3,16 +3,14 @@ require_relative '../spec_helper'
 module Kamerling describe Client do
   describe '.from_h' do
     it 'backtranslates host and port to addr' do
-      uuid = UUID.from_bin '16B client  UUID'
-      Client.from_h(host: '127.0.0.1', port: 1981, uuid: uuid)
-        .addr.must_equal Addr['127.0.0.1', 1981]
+      Client.from_h(host: '127.0.0.1', port: 1981, uuid: UUID.new).addr
+        .must_equal Addr['127.0.0.1', 1981]
     end
   end
 
   describe '#to_h' do
     it 'represents addr as a host + port pair' do
-      uuid = UUID.from_bin '16B client  UUID'
-      hash = Client[addr: Addr['127.0.0.1', 1981], uuid: uuid].to_h
+      hash = Client[addr: Addr['127.0.0.1', 1981], uuid: UUID.new].to_h
       hash[:host].must_equal '127.0.0.1'
       hash[:port].must_equal 1981
     end
