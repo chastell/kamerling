ab52a0aa43b6810b97910f6a99f2b07522ae57ad
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 19:28:04 +0200

    simplify Task spec

--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -1,20 +1,20 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Task do
+  fakes :project
+
   describe '.from_h' do
     it 'backtranslates a project_uuid to project' do
-      project = fake :project
-      repos   = { Project => { project.uuid => project } }
-      hash    = { input: 'input', project_uuid: project.uuid, uuid: UUID.new }
+      repos = { Project => { project.uuid => project } }
+      hash  = { input: 'input', project_uuid: project.uuid, uuid: UUID.new }
       Task.from_h(hash, repos).project.must_equal project
     end
   end
 
   describe '#to_h' do
     it 'represents project as its UUID' do
-      project = Project[name: 'project name', uuid: puuid = UUID.new]
-      task    = Task[input: 'task input', project: project, uuid: UUID.new]
-      task.to_h[:project_uuid].must_equal puuid
+      task = Task[input: 'task input', project: project, uuid: UUID.new]
+      task.to_h[:project_uuid].must_equal project.uuid
     end
   end
 end end
