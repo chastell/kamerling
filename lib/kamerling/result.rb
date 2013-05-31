369f777d44b9e6849e5f43ab784bfdb7b53fc5d3
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:31:48 +0200

    add Result#to_h

--- lib/kamerling/result.rb
+++ lib/kamerling/result.rb
@@ -4,5 +4,15 @@ module Kamerling
       uuid: UUID.new
       super addr, client, data, task, uuid
     end
+
+    def to_h
+      super.tap do |hash|
+        hash.merge! client_uuid: client.uuid, task_uuid: task.uuid
+        hash.merge! host: addr.host, port: addr.port
+        hash.delete :addr
+        hash.delete :client
+        hash.delete :task
+      end
+    end
   end
 end
--- spec/kamerling/result_spec.rb
+++ spec/kamerling/result_spec.rb
@@ -10,4 +10,12 @@ module Kamerling describe Result do
       r1.uuid.wont_equal r2.uuid
     end
   end
+
+  describe '#to_h' do
+    it 'serialises addr, client and task' do
+      hash = Result[addr: addr, client: client, data: 'data', task: task].to_h
+      hash.must_equal({ client_uuid: client.uuid, data: 'data', host: addr.host,
+        port: addr.port, task_uuid: task.uuid, uuid: anything })
+    end
+  end
 end end
