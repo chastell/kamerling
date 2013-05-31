b318279c3f24f2bc407f95c8aebcb383416ff238
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:42:50 +0200

    refactor Repos spec

--- spec/kamerling/repos_spec.rb
+++ spec/kamerling/repos_spec.rb
@@ -2,6 +2,7 @@ require_relative '../spec_helper'
 
 module Kamerling describe Repos do
   before { Repos.db = Sequel.sqlite }
+  let(:addr) { Addr['127.0.0.1', 1981] }
 
   describe '.<<' do
     it 'shuffles the object into the right repo' do
@@ -13,7 +14,7 @@ module Kamerling describe Repos do
 
   describe '.[]' do
     it 'allows querying for repository objects' do
-      client = Client[addr: Addr['127.0.0.1', 1981], uuid: uuid = UUID.new]
+      client = Client[addr: addr, uuid: uuid = UUID.new]
       Repos[Client, repo: {}][uuid].must_be_nil
       Repos[Client, repo: { uuid => client }][uuid].must_equal client
     end
@@ -28,7 +29,7 @@ module Kamerling describe Repos do
     end
 
     it 'makes sure clients can be stored and retrieved' do
-      client = Client[addr: Addr['127.0.0.1', 1981], uuid: uuid = UUID.new]
+      client = Client[addr: addr, uuid: uuid = UUID.new]
       Repos << client
       Repos[Client][uuid].must_equal client
     end
@@ -40,7 +41,6 @@ module Kamerling describe Repos do
     end
 
     it 'makes sure registrations can be stored and retrieved' do
-      addr = Addr['127.0.0.1', 1981]
       Repos << project = Project[name: 'project name', uuid: UUID.new]
       Repos << client = Client[addr: addr, uuid: UUID.new]
       Repos << reg = Registration[addr: addr, client: client, project: project]
@@ -48,7 +48,6 @@ module Kamerling describe Repos do
     end
 
     it 'makes sure results can be stored and retrieved' do
-      addr = Addr['127.0.0.1', 1981]
       Repos << project = Project[name: 'project name', uuid: UUID.new]
       Repos << client = Client[addr: addr, uuid: UUID.new]
       Repos << task = Task[input: 'input', project: project, uuid: UUID.new]
