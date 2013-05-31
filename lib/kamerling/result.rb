38af3c4fbda60c62b04cef3e023c946e3a53ebae
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:36:07 +0200

    add Result.from_h

--- lib/kamerling/result.rb
+++ lib/kamerling/result.rb
@@ -1,5 +1,16 @@
 module Kamerling
   Result = Struct.new :addr, :client, :data, :task, :uuid do
+    def self.from_h hash
+      hash.merge! addr:   Addr[hash[:host], hash[:port]]
+      hash.merge! client: Repos[Client][hash[:client_uuid]]
+      hash.merge! task:   Repos[Task][hash[:task_uuid]]
+      hash.delete :host
+      hash.delete :port
+      hash.delete :client_uuid
+      hash.delete :task_uuid
+      new hash
+    end
+
     def initialize addr: raise, client: raise, data: raise, task: raise,
       uuid: UUID.new
       super addr, client, data, task, uuid
--- spec/kamerling/result_spec.rb
+++ spec/kamerling/result_spec.rb
@@ -1,7 +1,22 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Result do
+  before { Repos.db = Sequel.sqlite }
+
   fakes :addr, :client, :task
+
+  describe '.from_h' do
+    it 'backtranslates client_uuid, host, port and task_uuid' do
+      addr = Addr['127.0.0.1', 1981]
+      Repos << client  = Client[addr: addr, uuid: UUID.new]
+      Repos << project = Project[name: 'project name', uuid: UUID.new]
+      Repos << task    = Task[input: 'input', project: project, uuid: UUID.new]
+      res = Result.from_h client_uuid: client.uuid, data: 'data',
+        host: '127.0.0.1', port: 1981, task_uuid: task.uuid
+      res.must_equal Result[addr: addr, client: client, data: 'data',
+        task: task, uuid: anything]
+    end
+  end
 
   describe '.new' do
     it 'gives the result a random UUID' do
