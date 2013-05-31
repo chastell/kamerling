d6ce96b4b3c87764f07b467b30574bad4a0643ff
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 16:59:13 +0200

    clean-up Task spec

--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -5,21 +5,17 @@ module Kamerling describe Task do
 
   describe '.from_h' do
     it 'backtranslates a project_uuid to project' do
-      project_uuid = UUID.from_bin '16B project UUID'
-      Repos << project = Project[name: 'project name', uuid: project_uuid]
-      hash = { input: 'input', project_uuid: project_uuid,
-        uuid: UUID.from_bin('16B task    UUID') }
+      Repos << project = Project[name: 'project name', uuid: puuid = UUID.new]
+      hash = { input: 'input', project_uuid: puuid, uuid: UUID.new }
       Task.from_h(hash).project.must_equal project
     end
   end
 
   describe '#to_h' do
     it 'represents project as its UUID' do
-      project_uuid = UUID.from_bin '16B project UUID'
-      project = Project[name: 'project name', uuid: project_uuid]
-      task    = Task[input: 'task input', project: project,
-        uuid: UUID.from_bin('16B task    UUID')]
-      task.to_h[:project_uuid].must_equal project_uuid
+      project = Project[name: 'project name', uuid: puuid = UUID.new]
+      task    = Task[input: 'task input', project: project, uuid: UUID.new]
+      task.to_h[:project_uuid].must_equal puuid
     end
   end
 end end
