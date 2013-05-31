42c5e765991a779dfb2ad38a7bb55b39e4179279
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 19:35:26 +0200

    simplify Project spec

--- spec/kamerling/project_spec.rb
+++ spec/kamerling/project_spec.rb
@@ -3,8 +3,8 @@ require_relative '../spec_helper'
 module Kamerling describe Project do
   describe '.from_h' do
     it 'deserialises a project from a hash' do
-      hash = { name: name = 'project name', uuid: uuid = UUID.new }
-      Project.from_h(hash).must_equal Project[name: name, uuid: uuid]
+      hash = { name: 'project name', uuid: uuid = UUID.new }
+      Project.from_h(hash).must_equal Project[name: 'project name', uuid: uuid]
     end
   end
 end end
