4b400e79cb94fdc5fa8d6965b22999ba2dd6fcc8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-05-31 18:45:13 +0200

    simplify Repos spec

--- spec/kamerling/repos_spec.rb
+++ spec/kamerling/repos_spec.rb
@@ -29,15 +29,13 @@ module Kamerling describe Repos do
     end
 
     it 'makes sure clients can be stored and retrieved' do
-      client = Client[addr: addr, uuid: uuid = UUID.new]
-      Repos << client
-      Repos[Client][uuid].must_equal client
+      Repos << client = Client[addr: addr, uuid: UUID.new]
+      Repos[Client][client.uuid].must_equal client
     end
 
     it 'makes sure projects can be stored and retrieved' do
-      project = Project[name: 'project name', uuid: uuid = UUID.new]
-      Repos << project
-      Repos[Project][uuid].must_equal project
+      Repos << project = Project[name: 'project name', uuid: UUID.new]
+      Repos[Project][project.uuid].must_equal project
     end
 
     it 'makes sure registrations can be stored and retrieved' do
@@ -56,11 +54,9 @@ module Kamerling describe Repos do
     end
 
     it 'makes sure tasks can be stored and retrieved' do
-      project = Project[name: 'project name', uuid: UUID.new]
-      task    = Task[input: 'input', project: project, uuid: tuuid = UUID.new]
-      Repos << project
-      Repos << task
-      Repos[Task][tuuid].must_equal task
+      Repos << project = Project[name: 'project name', uuid: UUID.new]
+      Repos << task = Task[input: 'input', project: project, uuid: UUID.new]
+      Repos[Task][task.uuid].must_equal task
     end
   end
 end end
