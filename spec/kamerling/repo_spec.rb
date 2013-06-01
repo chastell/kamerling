b206ae14ba1edd02d0e934ea236df9afb524f64e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-01 16:19:42 +0200

    simplify Repo spec

--- spec/kamerling/repo_spec.rb
+++ spec/kamerling/repo_spec.rb
@@ -1,25 +1,24 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Repo do
+  Tune = Struct.new :genre do
+    class << self
+      alias :from_h :[]
+    end
+    def initialize genre: raise
+      super genre
+    end
+  end
+
   describe '#<<' do
     it 'passes the Hash version of an object to the source' do
-      Pic = Struct.new :desc
-      pic = Pic[:sunset]
-      mock(source = fake) << { desc: :sunset }
-      Repo.new(source, Pic) << pic
+      tune = Tune[genre: :chap_hop]
+      mock(source = fake) << { genre: :chap_hop }
+      Repo.new(source, Tune) << tune
     end
   end
 
   describe '#[]' do
-    Tune = Struct.new :genre do
-      class << self
-        alias :from_h :[]
-      end
-      def initialize genre: raise
-        super genre
-      end
-    end
-
     it 'hydrates the object found in the repo' do
       source = { { uuid: uuid = UUID.new } => { genre: :chap_hop } }
       Repo.new(source, Tune)[uuid].must_equal Tune[genre: :chap_hop]
