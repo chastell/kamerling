db770c48d465ee6c389db0b87e611d8b1e5bd75b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-01 16:22:14 +0200

    Repo spec: add Tune#uuid

--- spec/kamerling/repo_spec.rb
+++ spec/kamerling/repo_spec.rb
@@ -1,27 +1,28 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Repo do
-  Tune = Struct.new :genre do
+  Tune = Struct.new :genre, :uuid do
     class << self
       alias :from_h :[]
     end
-    def initialize genre: raise
-      super genre
+    def initialize genre: raise, uuid: UUID.new
+      super genre, uuid
     end
   end
 
   describe '#<<' do
     it 'passes the Hash version of an object to the source' do
       tune = Tune[genre: :chap_hop]
-      mock(source = fake) << { genre: :chap_hop }
+      mock(source = fake) << { genre: :chap_hop, uuid: anything }
       Repo.new(source, Tune) << tune
     end
   end
 
   describe '#[]' do
     it 'hydrates the object found in the repo' do
-      source = { { uuid: uuid = UUID.new } => { genre: :chap_hop } }
-      Repo.new(source, Tune)[uuid].must_equal Tune[genre: :chap_hop]
+      uuid   = UUID.new
+      source = { { uuid: uuid } => { genre: :chap_hop, uuid: uuid } }
+      Repo.new(source, Tune)[uuid].must_equal Tune[genre: :chap_hop, uuid: uuid]
     end
 
     it 'raises NotFound if the object is not found in the repo' do
