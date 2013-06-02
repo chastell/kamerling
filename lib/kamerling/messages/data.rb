c7eeec90a0454791f6ca69210feff82ae46118ce
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-02 18:07:19 +0200

    add Messages::DATA.[]

--- lib/kamerling/messages/data.rb
+++ lib/kamerling/messages/data.rb
@@ -1,2 +1,7 @@
 module Kamerling module Messages class DATA < Message
+  def self.[] client: raise, data: raise, project: raise, task: raise
+    input = "DATA\0\0\0\0\0\0\0\0\0\0\0\0" + UUID.bin(client.uuid) +
+      UUID.bin(project.uuid) + UUID.bin(task.uuid) + data
+    new input
+  end
 end end end
--- spec/kamerling/messages/data_spec.rb
+++ spec/kamerling/messages/data_spec.rb
@@ -5,4 +5,17 @@ module Kamerling module Messages describe RGST do
   let(:mess) { DATA.new "DATA\0\0\0\0\0\0\0\0\0\0\0\0" +
     '16B client  UUID16B project UUID16B task    UUIDsome-length data' }
   include MessageBehaviour
+
+  describe '.[]' do
+    it 'constructs a new DATA message' do
+      client  = fake :client,  uuid: UUID.new
+      project = fake :project, uuid: UUID.new
+      task    = fake :task,    uuid: UUID.new
+      mess    = DATA[client: client, data: 'data', project: project, task: task]
+      mess.client_uuid.must_equal client.uuid
+      mess.project_uuid.must_equal project.uuid
+      mess.task_uuid.must_equal task.uuid
+      mess.data.must_equal 'data'
+    end
+  end
 end end end
