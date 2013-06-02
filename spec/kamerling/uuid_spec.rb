4fa29a15f2d4881700878c9e5b3f8d3af01dfad3
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-02 17:54:40 +0200

    add UUID.bin

--- spec/kamerling/uuid_spec.rb
+++ spec/kamerling/uuid_spec.rb
@@ -7,6 +7,13 @@ module Kamerling describe UUID do
     end
   end
 
+  describe '.bin' do
+    it 'returns the binary representation of an UUID' do
+      UUID.bin('31364220-7072-6f6a-6563-742055554944')
+        .must_equal '16B project UUID'
+    end
+  end
+
   describe '.new' do
     it 'creates a new random UUID' do
       UUID.new.must_match(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
