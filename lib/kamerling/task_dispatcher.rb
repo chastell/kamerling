51fa4f1feaccd69653d328e5133047b12bcda2f1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-02 18:55:07 +0200

    first cut of TaskDispatcher

---
+++ lib/kamerling/task_dispatcher.rb
@@ -0,0 +1,15 @@
+module Kamerling class TaskDispatcher
+  def dispatch repos: Repos
+    repos.projects.each do |project|
+      repos.free_clients_for(project).each do |client|
+        if task = repos.next_task_for(project)
+          TCPSocket.open(*client.addr) do |socket|
+            message = Messages::DATA[client: client, data: task.input,
+              project: project, task: task]
+            socket << message.input
+          end
+        end
+      end
+    end
+  end
+end end
---
+++ spec/kamerling/task_dispatcher_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper'
+
+module Kamerling describe TaskDispatcher do
+  describe '#dispatch' do
+    it 'dispatches tasks to free clients' do
+      endpoint = Server.new handler: handler = fake(:handler)
+      project  = fake :project, uuid: UUID['16B project UUID']
+      task     = fake :task, input: 'task input', project: project,
+        uuid: UUID['16B task    UUID']
+      client   = fake :client, addr: endpoint.addr, busy: false,
+        uuid: UUID['16B client  UUID']
+      repos = fake :repos, as: :class, projects: [project]
+      stub(repos).free_clients_for(project) { [client] }
+      stub(repos).next_task_for(project) { task }
+      TCPSocket.open(*endpoint.addr) do |socket|
+        TaskDispatcher.new.dispatch repos: repos
+      end
+      sleep 0.01
+      handler.must_have_received :handle, ["DATA\0\0\0\0\0\0\0\0\0\0\0\0" +
+        '16B client  UUID16B project UUID16B task    UUIDtask input', anything]
+    end
+  end
+end end
