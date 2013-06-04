f9d4f4fbb1a4d0c970bb9a85508d4cf15b799a5a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-04 09:02:16 +0200

    minor Receiver#receive refactoring

--- lib/kamerling/receiver.rb
+++ lib/kamerling/receiver.rb
@@ -3,10 +3,9 @@ module Kamerling class Receiver
     task_uuid: raise
     client = repos[Client][client_uuid]
     task   = repos[Task][task_uuid]
+    result = Result[addr: addr, client: client, data: data, task: task]
     client.busy = false
     task.done   = true
-    repos << Result[addr: addr, client: client, data: data, task: task]
-    repos << client
-    repos << task
+    repos << result << client << task
   end
 end end
