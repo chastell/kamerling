df73d06eeb372ef693bbb89705cfd132b338838b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:44:26 +0200

    add Task#done spec

--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -10,4 +10,10 @@ module Kamerling describe Task do
       t1.uuid.wont_equal t2.uuid
     end
   end
+
+  describe '#done' do
+    it 'defaults to false' do
+      Task[input: 'input', project: fake(:project)].done.must_equal false
+    end
+  end
 end end
