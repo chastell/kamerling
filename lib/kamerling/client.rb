2cc218f090b5678bb6c37429bc61136e0603aaa4
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:28:28 +0200

    Client.from_h: no need to custom-deserialise host and port

--- lib/kamerling/client.rb
+++ lib/kamerling/client.rb
@@ -1,8 +1,2 @@
 module Kamerling class Client < UUIDObject :addr, busy: false
-  def self.from_h hash
-    hash.merge! addr: Addr[hash[:host], hash[:port]]
-    hash.delete :host
-    hash.delete :port
-    new hash
-  end
 end end
--- spec/kamerling/client_spec.rb
+++ spec/kamerling/client_spec.rb
@@ -1,10 +1,4 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Client do
-  describe '.from_h' do
-    it 'backtranslates host and port to addr' do
-      Client.from_h(host: '127.0.0.1', port: 1981).addr
-        .must_equal Addr['127.0.0.1', 1981]
-    end
-  end
 end end
