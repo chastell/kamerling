ffedb9044137aab97e98bfeeed97aab36d6e9f47
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:40:40 +0200

    drop Project.from_h spec

--- spec/kamerling/project_spec.rb
+++ spec/kamerling/project_spec.rb
@@ -1,13 +1,6 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Project do
-  describe '.from_h' do
-    it 'deserialises a project from a hash' do
-      hash = { name: 'project name', uuid: uuid = UUID.new }
-      Project.from_h(hash).must_equal Project[name: 'project name', uuid: uuid]
-    end
-  end
-
   describe '.new' do
     it 'gives the project a random UUID if not provided' do
       p1 = Project[name: 'a']
