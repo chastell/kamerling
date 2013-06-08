362d88dd42cf34944c5cb44c14b81ebafbcb12a8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-08 21:31:45 +0200

    de-Struct Project

--- lib/kamerling/project.rb
+++ lib/kamerling/project.rb
@@ -1,11 +1,23 @@
-module Kamerling
-  Project = Struct.new :name, :uuid do
-    class << self
-      alias :from_h :new
-    end
-
-    def initialize name: req(:name), uuid: UUID.new
-      super name, uuid
-    end
+module Kamerling class Project
+  class << self
+    alias [] new
   end
-end
+
+  def self.from_h hash
+    new name: hash[:name], uuid: hash[:uuid]
+  end
+
+  attr_reader :name, :uuid
+
+  def initialize name: req(:name), uuid: UUID.new
+    @name, @uuid = name, uuid
+  end
+
+  def == other
+    uuid == other.uuid
+  end
+
+  def to_h
+    { name: name, uuid: uuid }
+  end
+end end
