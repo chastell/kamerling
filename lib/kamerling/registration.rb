3c9ad7782096d76db7df55f5d749bc345d21dd4a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:38:10 +0200

    Registration.from_h: no need to custom-deserialise

--- lib/kamerling/registration.rb
+++ lib/kamerling/registration.rb
@@ -1,12 +1,2 @@
 module Kamerling class Registration < UUIDObject :addr, :client, :project
-  def self.from_h hash, repos = Repos
-    hash.merge! addr:    Addr[hash[:host], hash[:port]]
-    hash.merge! client:  repos[Client][hash[:client_uuid]]
-    hash.merge! project: repos[Project][hash[:project_uuid]]
-    hash.delete :host
-    hash.delete :port
-    hash.delete :client_uuid
-    hash.delete :project_uuid
-    new hash
-  end
 end end
--- spec/kamerling/registration_spec.rb
+++ spec/kamerling/registration_spec.rb
@@ -3,18 +3,6 @@ require_relative '../spec_helper'
 module Kamerling describe Registration do
   fakes :addr, :client, :project
 
-  describe '.from_h' do
-    it 'backtranslates client_uuid, host, port and project_uuid' do
-      addr  = Addr['127.0.0.1', 1981]
-      repos = { Client => { client.uuid => client },
-        Project => { project.uuid => project } }
-      hash = { client_uuid: client.uuid, host: addr.host, port: addr.port,
-        project_uuid: project.uuid }
-      Registration.from_h(hash, repos).must_equal Registration[addr: addr,
-        client: client, project: project, uuid: anything]
-    end
-  end
-
   describe '.new' do
     it 'gives the registration a random UUID' do
       r1 = Registration[addr: addr, client: client, project: project]
