e5762804568d93f5cc7fd80ebf942252b68583ea
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-08 22:27:48 +0200

    make Result an UUIDObject

--- lib/kamerling/result.rb
+++ lib/kamerling/result.rb
@@ -1,28 +1,22 @@
-module Kamerling
-  Result = Struct.new :addr, :client, :data, :task, :uuid do
-    def self.from_h hash, repos = Repos
-      hash.merge! addr:   Addr[hash[:host], hash[:port]]
-      hash.merge! client: repos[Client][hash[:client_uuid]]
-      hash.merge! task:   repos[Task][hash[:task_uuid]]
-      hash.delete :host
-      hash.delete :port
-      hash.delete :client_uuid
-      hash.delete :task_uuid
-      new hash
-    end
+module Kamerling class Result < UUIDObject addr: -> { req :addr }, client: -> { req :client }, data: -> { req :data }, task: -> { req :task }
+  def self.from_h hash, repos = Repos
+    hash.merge! addr:   Addr[hash[:host], hash[:port]]
+    hash.merge! client: repos[Client][hash[:client_uuid]]
+    hash.merge! task:   repos[Task][hash[:task_uuid]]
+    hash.delete :host
+    hash.delete :port
+    hash.delete :client_uuid
+    hash.delete :task_uuid
+    new hash
+  end
 
-    def initialize addr: req(:addr), client: req(:client), data: req(:data), task: req(:task), uuid: UUID.new
-      super addr, client, data, task, uuid
-    end
-
-    def to_h
-      super.tap do |hash|
-        hash.merge! client_uuid: client.uuid, task_uuid: task.uuid
-        hash.merge! host: addr.host, port: addr.port
-        hash.delete :addr
-        hash.delete :client
-        hash.delete :task
-      end
+  def to_h
+    super.tap do |hash|
+      hash.merge! client_uuid: client.uuid, task_uuid: task.uuid
+      hash.merge! host: addr.host, port: addr.port
+      hash.delete :addr
+      hash.delete :client
+      hash.delete :task
     end
   end
-end
+end end
