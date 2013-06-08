31305f9b7b93fa825b0986ad24933e1901f931a9
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:44:40 +0200

    drop Task.new spec

--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -1,16 +1,6 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Task do
-  fakes :project
-
-  describe '.new' do
-    it 'gives the task a random UUID' do
-      t1 = Task[input: 'some input', project: project]
-      t2 = Task[input: 'some input', project: project]
-      t1.uuid.wont_equal t2.uuid
-    end
-  end
-
   describe '#done' do
     it 'defaults to false' do
       Task[input: 'input', project: fake(:project)].done.must_equal false
