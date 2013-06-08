1f2852b86ceb5556e94445a40578a1ae8e21f982
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:40:02 +0200

    spec Client#busy

--- spec/kamerling/client_spec.rb
+++ spec/kamerling/client_spec.rb
@@ -1,4 +1,9 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Client do
+  describe '#busy' do
+    it 'defaults to false' do
+      Client[addr: fake(:addr)].busy.must_equal false
+    end
+  end
 end end
