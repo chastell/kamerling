d1b45bee9d5b0ecaf9cb299f0507f776e8dc68d6
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-09 00:38:28 +0200

    Task.from_h: no need to custom-deserialise

--- lib/kamerling/task.rb
+++ lib/kamerling/task.rb
@@ -1,7 +1,2 @@
 module Kamerling class Task < UUIDObject :input, :project, done: false
-  def self.from_h hash, repos = Repos
-    hash.merge! project: repos[Project][hash[:project_uuid]]
-    hash.delete :project_uuid
-    new hash
-  end
 end end
--- spec/kamerling/task_spec.rb
+++ spec/kamerling/task_spec.rb
@@ -3,14 +3,6 @@ require_relative '../spec_helper'
 module Kamerling describe Task do
   fakes :project
 
-  describe '.from_h' do
-    it 'backtranslates a project_uuid to project' do
-      repos = { Project => { project.uuid => project } }
-      hash  = { input: 'input', project_uuid: project.uuid, uuid: UUID.new }
-      Task.from_h(hash, repos).project.must_equal project
-    end
-  end
-
   describe '.new' do
     it 'gives the task a random UUID' do
       t1 = Task[input: 'some input', project: project]
