0220d5d81bcf77e28eee4262cf927452d129342a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-22 17:42:33 +0200

    reimplement TaskDispatcher#dispatch spec

--- spec/kamerling/task_dispatcher_spec.rb
+++ spec/kamerling/task_dispatcher_spec.rb
@@ -2,24 +2,31 @@ require_relative '../spec_helper'
 
 module Kamerling describe TaskDispatcher do
   describe '#dispatch' do
-    it 'dispatches tasks to free clients' do
-      endpoint = Server.new(handler: handler = fake(:handler)).start
-      project  = fake :project, uuid: UUID['16B project UUID']
-      task     = fake :task, input: 'task input', project: project,
-        uuid: UUID['16B task    UUID']
-      client   = fake :client, addr: endpoint.addr, busy: false,
+    it 'dispatches tasks to free TCP clients' do
+      client  = fake :client, addr: Addr['127.0.0.1', 1980, 'TCP'], busy: false,
         uuid: UUID['16B client  UUID']
+      project = fake :project, uuid: UUID['16B project UUID']
+      task    = fake :task, input: 'task input', project: project,
+        uuid: UUID['16B task    UUID']
+
       repos = fake :repos, as: :class, projects: [project]
       stub(repos).free_clients_for(project) { [client] }
       stub(repos).next_task_for(project) { task }
-      TCPSocket.open endpoint.addr.host, endpoint.addr.port do |socket|
-        TaskDispatcher.new.dispatch repos: repos
+
+      message = nil
+      thread  = Thread.new do
+        socket  = TCPServer.new('127.0.0.1', 1980).accept
+        message = socket.read
+        socket.close
       end
       sleep 0.01
+      TaskDispatcher.new.dispatch repos: repos
+      thread.join
+
+      message.must_equal "DATA\0\0\0\0\0\0\0\0\0\0\0\0" +
+        '16B client  UUID16B project UUID16B task    UUIDtask input'
       client.must_have_received :busy=, true
       repos.must_have_received :<<, [client]
-      handler.must_have_received :handle, ["DATA\0\0\0\0\0\0\0\0\0\0\0\0" +
-        '16B client  UUID16B project UUID16B task    UUIDtask input', anything]
     end
   end
 end end
