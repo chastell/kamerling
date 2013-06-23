30afd82cc00aeeb70c4dc200cde12053f1def750
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-23 10:20:08 +0200

    simplify TaskDispatcher#dispatch spec

--- spec/kamerling/task_dispatcher_spec.rb
+++ spec/kamerling/task_dispatcher_spec.rb
@@ -3,23 +3,19 @@ require_relative '../spec_helper'
 module Kamerling describe TaskDispatcher do
   describe '#dispatch' do
     it 'dispatches tasks to free TCP clients' do
-      client  = fake :client, addr: Addr['127.0.0.1', 1980, 'TCP'], busy: false,
-        uuid: UUID['16B client  UUID']
+      server = TCPServer.open 0
+      addr   = Addr[server.addr[3], server.addr[1], 'TCP']
+
+      client  = fake :client, addr: addr, uuid: UUID['16B client  UUID']
       project = fake :project, uuid: UUID['16B project UUID']
-      task    = fake :task, input: 'task input', project: project,
-        uuid: UUID['16B task    UUID']
+      task    = fake :task, input: 'task input', uuid: UUID['16B task    UUID']
 
       repos = fake :repos, as: :class, projects: [project]
       stub(repos).free_clients_for(project) { [client] }
       stub(repos).next_task_for(project) { task }
 
       message = nil
-      thread  = Thread.new do
-        socket  = TCPServer.new('127.0.0.1', 1980).accept
-        message = socket.read
-        socket.close
-      end
-      sleep 0.01
+      thread  = Thread.new { message = server.accept.read }
       TaskDispatcher.new.dispatch repos: repos
       thread.join
 
