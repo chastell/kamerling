465e1181dcfcbd966428b0acc49dee601ce8f301
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-27 09:33:45 +0200

    Server: privatise port and rename to tcp_port

--- lib/kamerling/server.rb
+++ lib/kamerling/server.rb
@@ -1,26 +1,28 @@
 require 'gserver'
 
 module Kamerling class Server < GServer
-  def initialize handler: Handler.new, host: DEFAULT_HOST, port: 0
+  def initialize handler: Handler.new, host: DEFAULT_HOST, tcp_port: 0
     @handler = handler
-    super port, host
+    super tcp_port, host
   end
 
   def start
     start_udp_server
     super
+    @tcp_port = port
+    self
   end
 
   def tcp_addr
-    Addr[host, port, 'TCP']
+    Addr[host, tcp_port, 'TCP']
   end
 
   def udp_addr
-    Addr[host, port, 'UDP']
+    Addr[host, tcp_port, 'UDP']
   end
 
-  attr_reader :handler
-  private     :handler
+  attr_reader :handler, :tcp_port
+  private     :handler, :port, :tcp_port
 
   private
 
@@ -31,7 +33,7 @@ module Kamerling class Server < GServer
   def start_udp_server
     Thread.new do
       udp_server = UDPSocket.new
-      udp_server.bind host, port
+      udp_server.bind host, tcp_port
       loop do
         if IO.select [udp_server]
           input, conn = udp_server.recvfrom(2**16)
--- spec/kamerling/server_spec.rb
+++ spec/kamerling/server_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 module Kamerling describe Server do
   describe '.new' do
     it 'starts a server on the given host and port' do
-      Server.new(host: '0.0.0.0', port: 1981).start
+      Server.new(host: '0.0.0.0', tcp_port: 1981).start
       TCPSocket.open '0.0.0.0', 1981
     end
 
@@ -13,11 +13,11 @@ module Kamerling describe Server do
 
     it 'defaults to a random, unused port' do
       s1, s2 = Server.new.start, Server.new.start
-      (1024..65535).must_include s1.port
-      (1024..65535).must_include s2.port
-      s1.port.wont_equal s2.port
-      TCPSocket.open s1.host, s1.port
-      TCPSocket.open s2.host, s2.port
+      (1024..65535).must_include s1.tcp_addr.port
+      (1024..65535).must_include s2.tcp_addr.port
+      s1.tcp_addr.port.wont_equal s2.tcp_addr.port
+      TCPSocket.open s1.host, s1.tcp_addr.port
+      TCPSocket.open s2.host, s2.tcp_addr.port
     end
   end
 
@@ -25,7 +25,7 @@ module Kamerling describe Server do
     it 'listens on a TCP port and passes the received input to the handler' do
       server = Server.new(handler: handler = fake(:handler)).start
       s_addr = nil
-      TCPSocket.open server.host, server.port do |socket|
+      TCPSocket.open server.tcp_addr.host, server.tcp_addr.port do |socket|
         socket << 'message'
         s_addr = Addr[*socket.local_address.ip_unpack, 'TCP']
       end
@@ -37,7 +37,7 @@ module Kamerling describe Server do
       server = Server.new(handler: handler = fake(:handler)).start
       sleep 0.02
       client = UDPSocket.new
-      client.connect server.host, server.port
+      client.connect server.host, server.tcp_addr.port
       client.send 'message', 0
       c_addr = Addr[client.addr[3], client.addr[1], 'UDP']
       sleep 0.02
@@ -48,14 +48,14 @@ module Kamerling describe Server do
   describe '#tcp_addr' do
     it 'returns the server’s host + port as a TCP addr' do
       server = Server.new
-      server.tcp_addr.must_equal Addr[server.host, server.port, 'TCP']
+      server.tcp_addr.must_equal Addr[server.host, server.tcp_addr.port, 'TCP']
     end
   end
 
   describe '#udp_addr' do
     it 'returns the server’s host + port as an UDP addr' do
       server = Server.new
-      server.udp_addr.must_equal Addr[server.host, server.port, 'UDP']
+      server.udp_addr.must_equal Addr[server.host, server.tcp_addr.port, 'UDP']
     end
   end
 end end
