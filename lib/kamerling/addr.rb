50ab19cf812520827ef4708f3f40046bb96f94ad
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-06-28 10:31:53 +0200

    add Addr#to_a for splat use

--- lib/kamerling/addr.rb
+++ lib/kamerling/addr.rb
@@ -1,3 +1,7 @@
 module Kamerling
-  Addr = Struct.new :host, :port, :prot
+  Addr = Struct.new :host, :port, :prot do
+    def to_a
+      [host, port]
+    end
+  end
 end
---
+++ spec/kamerling/addr_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../spec_helper'
+
+module Kamerling describe Addr do
+  describe '#to_a' do
+    it 'returns host + port for splat use' do
+      splat = *Addr['127.0.0.1', 1981, 'TCP']
+      splat.must_equal ['127.0.0.1', 1981]
+    end
+  end
+end end
