e98c791a2ef76e4faefb1d3b8f110ca7e7e5564b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-07-09 14:26:19 +0200

    factor out TaskDispatcher#dispatch_task_to_client

--- lib/kamerling/task_dispatcher.rb
+++ lib/kamerling/task_dispatcher.rb
@@ -2,18 +2,22 @@ module Kamerling class TaskDispatcher
   def dispatch repos: Repos
     repos.projects.each do |project|
       repos.free_clients_for(project).each do |client|
-        if task = repos.next_task_for(project)
-          message = Messages::DATA[client: client, data: task.input,
-            project: project, task: task]
-          send "dispatch_to_#{client.addr.prot.downcase}", client, message
-          client.busy = true
-          repos << client
-        end
+        dispatch_task_to_client repos, project, client
       end
     end
   end
 
   private
+
+  def dispatch_task_to_client repos, project, client
+    if task = repos.next_task_for(project)
+      message = Messages::DATA[client: client, data: task.input,
+        project: project, task: task]
+      send "dispatch_to_#{client.addr.prot.downcase}", client, message
+      client.busy = true
+      repos << client
+    end
+  end
 
   def dispatch_to_tcp client, message
     TCPSocket.open(*client.addr) do |socket|
