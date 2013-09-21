9fa7ca4880b0b8cc91dcbae680300994878dc42d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-09-21 09:13:10 +0200

    drop Handler#parse altogether

--- lib/kamerling/handler.rb
+++ lib/kamerling/handler.rb
@@ -2,7 +2,7 @@ module Kamerling class Handler
   UnknownInput = Class.new RuntimeError
 
   def handle input, addr, receiver: Receiver.new, registrar: Registrar.new
-    message = parse input
+    message = Message.new input
     case message.type
     when 'RGST'
       registrar.register addr: addr, client_uuid: message.client_uuid,
@@ -12,11 +12,5 @@ module Kamerling class Handler
         data: message.payload, task_uuid: message.task_uuid
     else raise UnknownInput, input
     end
-  end
-
-  private
-
-  def parse input
-    Message.new input
   end
 end end
