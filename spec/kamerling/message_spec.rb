7069a3da0d25f4d0c3c4cdfaa7a68a9789e88452
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-09-22 10:54:43 +0200

    minor Message spec clarification

--- spec/kamerling/message_spec.rb
+++ spec/kamerling/message_spec.rb
@@ -14,13 +14,13 @@ module Kamerling describe Message do
       client  = fake :client,  uuid: UUID.new
       project = fake :project, uuid: UUID.new
       task    = fake :task,    uuid: UUID.new
-      mess    = Message[client: client, payload: 'pay', project: project,
+      message = Message[client: client, payload: 'pay', project: project,
         task: task, type: 'MESS']
-      mess.client_uuid.must_equal client.uuid
-      mess.project_uuid.must_equal project.uuid
-      mess.task_uuid.must_equal task.uuid
-      mess.payload.must_equal 'pay'
-      mess.type.must_equal 'MESS'
+      message.client_uuid.must_equal client.uuid
+      message.project_uuid.must_equal project.uuid
+      message.task_uuid.must_equal task.uuid
+      message.payload.must_equal 'pay'
+      message.type.must_equal 'MESS'
     end
   end
 end end
