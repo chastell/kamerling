fe3b2d26e2029f6928dd246ab2e9893d2dd7ed47
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-09-23 10:33:46 +0200

    simplify Message.[] a bit

--- lib/kamerling/message.rb
+++ lib/kamerling/message.rb
@@ -3,9 +3,8 @@ module Kamerling class Message
 
   def self.[] client: req(:client), payload: req(:payload),
               project: req(:project), task: req(:task), type: req(:type)
-    raw = "#{type}\0\0\0\0\0\0\0\0\0\0\0\0" + UUID.bin(client.uuid) +
+    new "#{type}\0\0\0\0\0\0\0\0\0\0\0\0" + UUID.bin(client.uuid) +
       UUID.bin(project.uuid) + UUID.bin(task.uuid) + payload
-    new raw
   end
 
   def initialize raw
