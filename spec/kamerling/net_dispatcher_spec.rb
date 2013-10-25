6d145c7340b7d2a5dc4fedfeb8bdc473dfb599dc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-10-25 15:59:00 +0200

    simplify NetDispatcher spec

--- spec/kamerling/net_dispatcher_spec.rb
+++ spec/kamerling/net_dispatcher_spec.rb
@@ -1,20 +1,21 @@
 require_relative '../spec_helper'
 
 module Kamerling describe NetDispatcher do
-  let(:tcp_server) { TCPServer.open 0                                }
-  let(:udp_server) { UDPSocket.new.tap { |s| s.bind '127.0.0.1', 0 } }
-  let(:tcp_thread) { Thread.new { tcp_server.accept.read }           }
-  let(:udp_thread) { Thread.new { udp_server.recvfrom(2**16).first } }
-
   describe '#dispatch' do
-    [:TCP, :UDP].each do |type|
-      it "dispatches messages to #{type} clients" do
-        server = send "#{type.downcase}_server"
-        thread = send "#{type.downcase}_thread"
-        addr   = Addr[server.addr[3], server.addr[1], type]
-        NetDispatcher.new(addr).dispatch 'foo'
-        thread.value.must_equal 'foo'
-      end
+    it 'dispatches messages to TCP clients' do
+      server = TCPServer.open 0
+      thread = Thread.new { server.accept.read }
+      addr   = Addr[server.addr[3], server.addr[1], :TCP]
+      NetDispatcher.new(addr).dispatch 'foo'
+      thread.value.must_equal 'foo'
+    end
+
+    it 'dispatches messages to UDP clients' do
+      server = UDPSocket.new.tap { |s| s.bind '127.0.0.1', 0 }
+      thread = Thread.new { server.recvfrom(2**16).first }
+      addr   = Addr[server.addr[3], server.addr[1], :UDP]
+      NetDispatcher.new(addr).dispatch 'foo'
+      thread.value.must_equal 'foo'
     end
   end
 end end
