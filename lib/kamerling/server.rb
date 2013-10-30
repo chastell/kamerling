a6023f7d8cfd950f8108b0785b088f0094c59d91
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-10-30 18:45:48 +0100

    Server: simplify ivar names

--- lib/kamerling/server.rb
+++ lib/kamerling/server.rb
@@ -5,30 +5,30 @@ module Kamerling class Server
   def initialize(handler: Handler.new, host: '127.0.0.1',
                  logger: Logger.new('/dev/null'), tcp: nil, tcp_port: 0,
                  udp: nil, udp_port: 0)
-    @tcp_server = tcp || TCP.new(handler: handler, host: host, logger: logger,
+    @tcp = tcp || TCP.new(handler: handler, host: host, logger: logger,
       port: tcp_port)
-    @udp_server = udp || UDP.new(handler: handler, host: host, logger: logger,
+    @udp = udp || UDP.new(handler: handler, host: host, logger: logger,
       port: udp_port)
   end
 
   def join
-    tcp_server.join
+    tcp.join
   end
 
   def start
-    udp_server.start
-    tcp_server.start
+    tcp.start
+    udp.start
     self
   end
 
   def tcp_addr
-    tcp_server.addr
+    tcp.addr
   end
 
   def udp_addr
-    udp_server.addr
+    udp.addr
   end
 
-  attr_reader :udp_server, :tcp_server
-  private     :udp_server, :tcp_server
+  attr_reader :tcp, :udp
+  private     :tcp, :udp
 end end
