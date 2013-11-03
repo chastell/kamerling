44750d93c6461ff03e7ddd2d4052895e3e2bf41c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-03 11:38:43 +0100

    Server::HTTP spec: minor refactoring

--- spec/kamerling/server/http_spec.rb
+++ spec/kamerling/server/http_spec.rb
@@ -5,14 +5,13 @@ require_relative '../../spec_helper'
 module Kamerling describe Server::HTTP do
   describe '#start, #stop' do
     it 'starts/stops a HTTP server on the given host and port' do
-      response = nil
       capture_io do
         server = Server::HTTP.new(host: '0.0.0.0', port: 2009).start
         400.times { run_all_threads }
-        response = Net::HTTP.get_response URI.parse 'http://0.0.0.0:2009'
+        uri = URI.parse 'http://0.0.0.0:2009'
+        Net::HTTP.get_response(uri).must_be_kind_of Net::HTTPSuccess
         server.stop
       end
-      response.must_be_kind_of Net::HTTPSuccess
     end
   end
 end end
