2ad4e2b776ab15161615f50a266a7b0ba02f1ebc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-04 20:47:39 +0100

    HTTPAPI: because we can

--- lib/kamerling/http_api.rb
+++ lib/kamerling/http_api.rb
@@ -2,17 +2,22 @@ require 'sinatra/base'
 require 'slim'
 
 module Kamerling class HTTPAPI < Sinatra::Base
+  enable :inline_templates
+
   get '/' do
-    slim <<-end.gsub(/^ {6}/, '')
-      doctype html
-      html
-        head
-          title Kamerling
-        body
-          nav
-            ul
-              li
-                a href='/projects' id='projects' projects
-    end
+    slim :root
   end
 end end
+
+__END__
+
+@@ root
+doctype html
+html
+  head
+    title Kamerling
+  body
+    nav
+      ul
+        li
+          a href='/projects' id='projects' projects
