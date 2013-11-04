9f20b2dee85ff136ee0bbb4256d8e1987e361ebb
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-04 09:20:25 +0100

    first HTTPAPI steps

--- lib/kamerling/http_api.rb
+++ lib/kamerling/http_api.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 module Kamerling class HTTPAPI < Sinatra::Base
   get '/' do
+    <<-end.gsub(/^ {6}/, '')
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Kamerling</title>
+        </head>
+        <body>
+          <nav>
+            <ul>
+              <li><a href='/projects' id='projects'>projects</a></li>
+            </ul>
+          </nav>
+        </body>
+      </html>
+    end
   end
 end end
--- spec/kamerling/http_api_spec.rb
+++ spec/kamerling/http_api_spec.rb
@@ -1,10 +1,15 @@
+require 'nokogiri'
+
 require_relative '../spec_helper'
 
 module Kamerling describe HTTPAPI do
   let(:app) { HTTPAPI }
 
-  it 'serves root' do
-    get '/'
-    last_response.must_be :ok?
+  describe '/' do
+    it 'contains a link to projects' do
+      get '/'
+      doc = Nokogiri::HTML last_response.body
+      doc.css('#projects').first['href'].must_equal '/projects'
+    end
   end
 end end
