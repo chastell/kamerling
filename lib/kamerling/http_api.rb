d9f2a2fd9e1f98e9a57f57a2f68c3238e874704b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-04 20:49:31 +0100

    HTTPAPI: srsly, though, I’d miss Slim syntax colouring

--- lib/kamerling/http_api.rb
+++ lib/kamerling/http_api.rb
@@ -2,22 +2,7 @@ require 'sinatra/base'
 require 'slim'
 
 module Kamerling class HTTPAPI < Sinatra::Base
-  enable :inline_templates
-
   get '/' do
     slim :root
   end
 end end
-
-__END__
-
-@@ root
-doctype html
-html
-  head
-    title Kamerling
-  body
-    nav
-      ul
-        li
-          a href='/projects' id='projects' projects
