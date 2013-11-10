852fba1b3c43af37a0aa508b660fc655518cc5ad
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-10 09:18:15 +0100

    add Addr#connectable?

--- lib/kamerling/addr.rb
+++ lib/kamerling/addr.rb
@@ -1,5 +1,12 @@
 module Kamerling
   Addr = Struct.new :host, :port, :prot do
+    def connectable?
+      TCPSocket.open(*self).close
+      true
+    rescue Errno::ECONNREFUSED
+      false
+    end
+
     def to_a
       [host, port]
     end
--- spec/kamerling/addr_spec.rb
+++ spec/kamerling/addr_spec.rb
@@ -1,6 +1,16 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Addr do
+  describe '#connectable?' do
+    it 'is a predicate whether the (TCP) address is connectable' do
+      addr   = Addr['localhost', 1981, :TCP]
+      server = TCPServer.new(*addr)
+      addr.must_be :connectable?
+      server.close
+      addr.wont_be :connectable?
+    end
+  end
+
   describe '#to_a' do
     it 'returns host + port for splat use' do
       splat = *Addr['127.0.0.1', 1981, :TCP]
