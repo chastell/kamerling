f30ae826cb26188a9bf7f1f08f51d61c801c31ba
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-10 20:37:44 +0100

    minor Addr spec refactoring

--- spec/kamerling/addr_spec.rb
+++ spec/kamerling/addr_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Addr do
+  let(:addr) { Addr['localhost', 1981, :TCP] }
+
   describe '#connectable?' do
     it 'is a predicate whether the (TCP) address is connectable' do
-      addr   = Addr['localhost', 1981, :TCP]
       server = TCPServer.new(*addr)
       addr.must_be :connectable?
       server.close
@@ -13,14 +14,14 @@ module Kamerling describe Addr do
 
   describe '#to_a' do
     it 'returns host + port for splat use' do
-      splat = *Addr['127.0.0.1', 1981, :TCP]
-      splat.must_equal ['127.0.0.1', 1981]
+      splat = *addr
+      splat.must_equal ['localhost', 1981]
     end
   end
 
   describe '#to_s' do
     it 'returns the Addr in ‘host:port (protocol)’ notation' do
-      Addr['127.0.0.1', 1981, :UDP].to_s.must_equal '127.0.0.1:1981 (UDP)'
+      addr.to_s.must_equal 'localhost:1981 (TCP)'
     end
   end
 end end
