9fd5173ce2ff7c791f70e4114e78cf4eda4cd077
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-12 12:23:56 +0100

    refactor HTTPAPI spec

--- spec/kamerling/http_api_spec.rb
+++ spec/kamerling/http_api_spec.rb
@@ -3,7 +3,9 @@ require 'nokogiri'
 require_relative '../spec_helper'
 
 module Kamerling describe HTTPAPI do
-  let(:app) { HTTPAPI                           }
+  fake :repos, as: :class
+
+  let(:app) { HTTPAPI.set repos: repos          }
   let(:doc) { Nokogiri::HTML last_response.body }
 
   describe 'GET /' do
@@ -17,7 +19,7 @@ module Kamerling describe HTTPAPI do
     it 'contains links to projects' do
       gimps = fake :project, uuid: UUID.new
       ecc   = fake :project, uuid: UUID.new
-      app.set repos: fake(:repos, as: :class, projects: [gimps, ecc])
+      stub(repos).projects { [gimps, ecc] }
       get '/projects'
       links = doc.css('#projects a[rel=project]').map { |a| a['href'] }
       links.must_include "/projects/#{gimps.uuid}"
@@ -26,17 +28,15 @@ module Kamerling describe HTTPAPI do
   end
 
   describe 'POST /projects' do
-    it 'creates a new project with a random UUID' do
-      app.set repos: repos = fake(:repos, as: :class)
+    it 'creates a new project with the given name and UUID' do
+      post '/projects', name: 'ECC', uuid: uuid = UUID.new
+      repos.must_have_received :<<, [Project.new(name: 'ECC', uuid: uuid)]
+    end
+
+    it 'creates a new project with a random UUID if missing' do
       post '/projects', name: 'ECC'
       project = Project.new name: 'ECC', uuid: any(String)
       repos.must_have_received :<<, [project]
-    end
-
-    it 'creates a new project with the given UUID' do
-      app.set repos: repos = fake(:repos, as: :class)
-      post '/projects', name: 'ECC', uuid: uuid = UUID.new
-      repos.must_have_received :<<, [Project.new(name: 'ECC', uuid: uuid)]
     end
 
     it 'redirects to /projects' do
