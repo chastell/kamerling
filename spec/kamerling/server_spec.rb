1ae0c5865c1e2ba264b24a630fbabd7f4ab5f4ed
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-18 10:45:30 +0100

    Server spec: drop dead code

--- spec/kamerling/server_spec.rb
+++ spec/kamerling/server_spec.rb
@@ -1,15 +1,6 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Server do
-  let(:addrs) do
-    {
-      http: Addr['0.0.0.0', 2006, :TCP],
-      tcp:  Addr['0.0.0.0', 1981, :TCP],
-      udp:  Addr['0.0.0.0', 1979, :UDP],
-    }
-  end
-
-  let(:server)  { Server.new addrs: addrs }
   let(:servers) { [fake(Server::HTTP), fake(Server::TCP), fake(Server::UDP)] }
 
   describe '#join' do
