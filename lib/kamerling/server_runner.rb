aed6137ebbb1976ca99262c6d0d714fead3a607e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-19 18:35:56 +0100

    add ServerRunner

---
+++ lib/kamerling/server_runner.rb
@@ -0,0 +1,38 @@
+require 'optparse'
+
+module Kamerling class ServerRunner
+  Settings = Struct.new(*%i[host http tcp udp])
+
+  def initialize args, http: Server::HTTP, tcp: Server::TCP, udp: Server::UDP
+    @args = args
+    servers << http.new(addr: Addr[settings.host, settings.http, :TCP])
+    servers << tcp.new(addr:  Addr[settings.host, settings.tcp,  :TCP])
+    servers << udp.new(addr:  Addr[settings.host, settings.udp,  :UDP])
+  end
+
+  def start
+    servers.each(&:start)
+  end
+
+  attr_reader :args
+  private     :args
+
+  private
+
+  def servers
+    @servers ||= []
+  end
+
+  def settings
+    @settings ||= Settings.new.tap do |sets|
+      OptionParser.new do |opts|
+        opts.on('--host 127.0.0.1', String, 'server host') do |host|
+          sets.host = host
+        end
+        opts.on('--http 0', Integer, 'HTTP port') { |http| sets.http = http }
+        opts.on('--tcp 0',  Integer, 'TCP port')  { |tcp|  sets.tcp  = tcp  }
+        opts.on('--udp 0',  Integer, 'UDP port')  { |udp|  sets.udp  = udp  }
+      end.parse! args
+    end
+  end
+end end
---
+++ spec/kamerling/server_runner_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../spec_helper'
+
+module Kamerling describe ServerRunner do
+  describe '#start' do
+    it 'starts the servers based on the given command-line parameters' do
+      args    = %w[--host 0.0.0.0 --http 1234 --tcp 3456 --udp 5678]
+      http    = fake { Server::HTTP }
+      tcp     = fake { Server::TCP  }
+      udp     = fake { Server::UDP  }
+      http_cl = fake(as: :class) { Server::HTTP }
+      tcp_cl  = fake(as: :class) { Server::TCP  }
+      udp_cl  = fake(as: :class) { Server::UDP  }
+      stub(http_cl).new(addr: Addr['0.0.0.0', 1234, :TCP]) { http }
+      stub(tcp_cl).new(addr:  Addr['0.0.0.0', 3456, :TCP]) { tcp  }
+      stub(udp_cl).new(addr:  Addr['0.0.0.0', 5678, :UDP]) { udp  }
+      ServerRunner.new(args, http: http_cl, tcp: tcp_cl, udp: udp_cl).start
+      http.must_have_received :start, []
+      tcp.must_have_received :start,  []
+      udp.must_have_received :start,  []
+    end
+  end
+end end
