54d9f950e1f58ef38cfb39df4484a6dcbb32fe4e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2013-11-21 09:03:53 +0100

    refactor ServerRunner spec

--- spec/kamerling/server_runner_spec.rb
+++ spec/kamerling/server_runner_spec.rb
@@ -2,18 +2,22 @@ require_relative '../spec_helper'
 
 module Kamerling describe ServerRunner do
   describe '#start' do
-    it 'starts the servers based on the given command-line parameters' do
-      args    = %w[--host 0.0.0.0 --http 1234 --tcp 3456 --udp 5678]
-      http    = fake { Server::HTTP }
-      tcp     = fake { Server::TCP  }
-      udp     = fake { Server::UDP  }
-      http_cl = fake(as: :class) { Server::HTTP }
-      tcp_cl  = fake(as: :class) { Server::TCP  }
-      udp_cl  = fake(as: :class) { Server::UDP  }
+    let(:http)    { fake { Server::HTTP }                       }
+    let(:tcp)     { fake { Server::TCP  }                       }
+    let(:udp)     { fake { Server::UDP  }                       }
+    let(:http_cl) { fake(as: :class) { Server::HTTP }           }
+    let(:tcp_cl)  { fake(as: :class) { Server::TCP  }           }
+    let(:udp_cl)  { fake(as: :class) { Server::UDP  }           }
+    let(:classes) { { http: http_cl, tcp: tcp_cl, udp: udp_cl } }
+
+    before do
       stub(http_cl).new(addr: Addr['0.0.0.0', 1234, :TCP]) { http }
       stub(tcp_cl).new(addr:  Addr['0.0.0.0', 3456, :TCP]) { tcp  }
       stub(udp_cl).new(addr:  Addr['0.0.0.0', 5678, :UDP]) { udp  }
-      classes = { http: http_cl, tcp: tcp_cl, udp: udp_cl }
+    end
+
+    it 'starts the servers based on the given command-line parameters' do
+      args = %w[--host 0.0.0.0 --http 1234 --tcp 3456 --udp 5678]
       ServerRunner.new(args, classes: classes).start
       http.must_have_received :start, []
       tcp.must_have_received :start,  []
@@ -21,17 +25,7 @@ module Kamerling describe ServerRunner do
     end
 
     it 'starts only the servers for which the port was given' do
-      args    = %w[--host 0.0.0.0 --http 1234]
-      http    = fake { Server::HTTP }
-      tcp     = fake { Server::TCP  }
-      udp     = fake { Server::UDP  }
-      http_cl = fake(as: :class) { Server::HTTP }
-      tcp_cl  = fake(as: :class) { Server::TCP  }
-      udp_cl  = fake(as: :class) { Server::UDP  }
-      stub(http_cl).new(addr: Addr['0.0.0.0', 1234, :TCP]) { http }
-      stub(tcp_cl).new(addr:  Addr['0.0.0.0', 3456, :TCP]) { tcp  }
-      stub(udp_cl).new(addr:  Addr['0.0.0.0', 5678, :UDP]) { udp  }
-      classes = { http: http_cl, tcp: tcp_cl, udp: udp_cl }
+      args = %w[--host 0.0.0.0 --http 1234]
       ServerRunner.new(args, classes: classes).start
       http.must_have_received :start, []
       tcp.wont_have_received :start,  []
