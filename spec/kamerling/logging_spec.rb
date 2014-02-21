2fd56711f8f8e2f1a6ef8fbfe6c79f6d41ad0320
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-02-21 17:37:46 +0100

    refactor Logging spec

--- spec/kamerling/logging_spec.rb
+++ spec/kamerling/logging_spec.rb
@@ -1,23 +1,21 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Logging do
+  let(:logged) { stream.tap(&:rewind).read }
+  let(:logger) { Logger.new stream         }
+  let(:stream) { StringIO.new              }
+
+  before { Logging.new logger: logger }
+
   describe '.new' do
     it 'logs TCP server starts' do
-      stream = StringIO.new
-      Logging.new logger: Logger.new(stream)
-      server = Server::TCP.new addr: Addr['localhost', 1981, :TCP]
-      server.start.stop
-      stream.rewind
-      stream.read.must_include 'start localhost:1981 (TCP)'
+      Server::TCP.new(addr: Addr['localhost', 1981, :TCP]).start.stop
+      logged.must_include 'start localhost:1981 (TCP)'
     end
 
     it 'logs TCP server stops' do
-      stream = StringIO.new
-      Logging.new logger: Logger.new(stream)
-      server = Server::TCP.new addr: Addr['localhost', 1981, :TCP]
-      server.start.stop
-      stream.rewind
-      stream.read.must_include 'stop localhost:1981 (TCP)'
+      Server::TCP.new(addr: Addr['localhost', 1981, :TCP]).start.stop
+      logged.must_include 'stop localhost:1981 (TCP)'
     end
   end
 end end
