891718a62c90ce0ce3c3baf72d1ea45d5bfdf11c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-03-06 09:19:47 +0100

    UUIDObject: try to split it up; this is bad and I feel bad

--- lib/kamerling/uuid_object.rb
+++ lib/kamerling/uuid_object.rb
@@ -16,38 +16,28 @@ module Kamerling
     end
 
     def class_definition_from attrs
-      Class.new do
+      new_class = Class.new
+      new_class.class_eval do
         define_singleton_method(:attrs) { attrs }
+        attrs.each do |attr, _|
+          define_method(attr)       { @values[attr]             }
+          define_method("#{attr}=") { |val| @values[attr] = val }
+        end
+      end
+      define_from_h_on new_class
+      define_initialize_on new_class
+      define_eq_on new_class
+      define_to_h_on new_class
+      new_class
+    end
 
+    def define_from_h_on new_class
+      new_class.class_eval do
         def self.from_h hash, repos: Repos
           args = hash.reduce({}) do |result, (key, _)|
             result.merge from_h_mapping hash, key, repos
           end
           new args
-        end
-
-        def initialize args = {}
-          @values = Hash[self.class.attrs.map do |attr, default|
-            value = args.fetch attr do
-              default.respond_to?(:call) ? default.call : default
-            end
-            [attr, value]
-          end]
-        end
-
-        def == other
-          uuid == other.uuid
-        end
-
-        attrs.each do |attr, _|
-          define_method(attr)       { @values[attr]             }
-          define_method("#{attr}=") { |val| @values[attr] = val }
-        end
-
-        def to_h
-          self.class.attrs.reduce({}) do |hash, (attr, _)|
-            hash.merge to_h_mapping attr
-          end
         end
 
         private
@@ -62,6 +52,39 @@ module Kamerling
           else { key => hash[key] }
           end
         end
+      end
+    end
+
+    def define_initialize_on new_class
+      new_class.class_eval do
+        def initialize args = {}
+          @values = Hash[self.class.attrs.map do |attr, default|
+            value = args.fetch attr do
+              default.respond_to?(:call) ? default.call : default
+            end
+            [attr, value]
+          end]
+        end
+      end
+    end
+
+    def define_eq_on new_class
+      new_class.class_eval do
+        def == other
+          uuid == other.uuid
+        end
+      end
+    end
+
+    def define_to_h_on new_class
+      new_class.class_eval do
+        def to_h
+          self.class.attrs.reduce({}) do |hash, (attr, _)|
+            hash.merge to_h_mapping attr
+          end
+        end
+
+        private
 
         def to_h_mapping attr
           case value = @values[attr]
