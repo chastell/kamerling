278d93dbe172686b2b7044b1df16bdf0b729e449
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-03-23 21:55:05 +0100

    Client: switch to UUIDEntity

--- lib/kamerling/client.rb
+++ lib/kamerling/client.rb
@@ -1,3 +1,14 @@
-module Kamerling
-  Client = UUIDObject.new :addr, busy: false
-end
+module Kamerling class Client < UUIDEntity
+  attribute :addr, Addr
+  attribute :busy, Boolean, default: false
+
+  def self.from_h hash
+    super.tap do |client|
+      client.addr = Addr[hash[:host], hash[:port], hash[:prot]]
+    end
+  end
+
+  def to_h
+    super.reject { |key, _| key == :addr }.merge addr.to_h
+  end
+end end
