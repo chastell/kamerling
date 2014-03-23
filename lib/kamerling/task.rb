312b6b3a64623aa53e7e80393fd4a3206f86d0c3
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-03-23 21:43:44 +0100

    Task: switch to UUIDEntity

--- lib/kamerling/task.rb
+++ lib/kamerling/task.rb
@@ -1,3 +1,9 @@
-module Kamerling
-  Task = UUIDObject.new :data, :project, done: false
-end
+module Kamerling class Task < UUIDEntity
+  attribute :data,    String
+  attribute :done,    Boolean, default: false
+  attribute :project, Project
+
+  def to_h
+    super.reject { |key, _| key == :project }.merge project_uuid: project.uuid
+  end
+end end
