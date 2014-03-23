0c6d83c767b44e82e76fbf525b1ac66489d0f70d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-03-23 22:03:12 +0100

    Result: switch to UUIDEntity

--- lib/kamerling/result.rb
+++ lib/kamerling/result.rb
@@ -1,3 +1,13 @@
-module Kamerling
-  Result = UUIDObject.new :addr, :client, :data, :task
-end
+module Kamerling class Result < UUIDEntity
+  attribute :addr,   Addr
+  attribute :client, Client
+  attribute :data,   String
+  attribute :task,   Task
+
+  def to_h
+    super
+      .reject { |key, _| key == :addr   }.merge(addr.to_h)
+      .reject { |key, _| key == :client }.merge(client_uuid: client.uuid)
+      .reject { |key, _| key == :task   }.merge task_uuid:   task.uuid
+  end
+end end
