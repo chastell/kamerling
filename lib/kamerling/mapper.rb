a3b7c4f2fee9e4bc41871395b4ab5f54b0d2e76b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-03-26 16:26:07 +0100

    Mapper: just .to_h for now, thanks

--- lib/kamerling/mapper.rb
+++ lib/kamerling/mapper.rb
@@ -2,15 +2,4 @@ module Kamerling class Mapper
   def self.to_h object
     object.to_h
   end
-
-  def initialize client
-    @client = client
-  end
-
-  def to_h
-    client.to_h
-  end
-
-  attr_reader :client
-  private     :client
 end end
--- spec/kamerling/mapper_spec.rb
+++ spec/kamerling/mapper_spec.rb
@@ -1,12 +1,11 @@
 require_relative '../spec_helper'
 
 module Kamerling describe Mapper do
-  describe '#to_h' do
+  describe '.to_h' do
     it 'returns Hash representation of a Client' do
       client = Client.new addr: Addr['127.0.0.1', 1979, :TCP], busy: true
-      mapper = Mapper.new client
-      mapper.to_h.must_equal busy: true, host: '127.0.0.1',
-                             port: 1979, prot: 'TCP', uuid: client.uuid
+      Mapper.to_h(client).must_equal busy: true, host: '127.0.0.1',
+                                     port: 1979, prot: 'TCP', uuid: client.uuid
     end
   end
 end end
