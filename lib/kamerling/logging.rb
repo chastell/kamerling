07012cb6e0a99a28177221adf344e6a7d4f132eb
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-04-01 07:46:51 +0200

    turn Logging into a class

--- lib/kamerling/logging.rb
+++ lib/kamerling/logging.rb
@@ -1,32 +1,36 @@
 require 'after_do'
 require 'logger'
 
-module Kamerling module Logging
-  module_function
-
-  def log_to logger = Logger.new($stdout)
-    log_dispatcher_to logger
-    log_server_to     logger
+module Kamerling class Logging
+  def self.log_to logger = Logger.new($stdout)
+    new logger
   end
 
-  class << self
-    private
+  def initialize logger
+    @logger = logger
+    log_dispatcher
+    log_server
+  end
 
-    def log_dispatcher_to logger
-      NetDispatcher.singleton_class.extend AfterDo
-      NetDispatcher.singleton_class.before :dispatch do |addr, bytes|
-        logger.debug "sent #{addr} #{bytes}"
-      end
+  attr_reader :logger
+  private     :logger
+
+  private
+
+  def log_dispatcher
+    NetDispatcher.singleton_class.extend AfterDo
+    NetDispatcher.singleton_class.before :dispatch do |addr, bytes|
+      logger.debug "sent #{addr} #{bytes}"
     end
+  end
 
-    def log_server_to logger
-      Server::Sock.extend AfterDo
-      Server::Sock.before(:start) { |srv| logger.info "start #{srv.addr}" }
-      Server::Sock.after(:stop)   { |srv| logger.info "stop #{srv.addr}"  }
-      Server::Sock.before :handle do |input, client_addr|
-        logger.info "connect #{client_addr}"
-        logger.debug "received #{client_addr} #{input}"
-      end
+  def log_server
+    Server::Sock.extend AfterDo
+    Server::Sock.before(:start) { |srv| logger.info "start #{srv.addr}" }
+    Server::Sock.after(:stop)   { |srv| logger.info "stop #{srv.addr}"  }
+    Server::Sock.before :handle do |input, client_addr|
+      logger.info "connect #{client_addr}"
+      logger.debug "received #{client_addr} #{input}"
     end
   end
 end end
