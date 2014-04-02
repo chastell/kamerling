2280a7ca58463adf377eca4a4685e2cff5914b75
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-04-02 07:28:36 +0200

    ServerRunner: refactor Settings

--- lib/kamerling/server_runner.rb
+++ lib/kamerling/server_runner.rb
@@ -2,7 +2,7 @@ require 'optparse'
 
 module Kamerling class ServerRunner
   def initialize args, classes: def_classes, orm: Sequel, repos: Repos
-    @args    = args
+    settings = Settings.new args
     repos.db = orm.connect settings.db
     @servers = { http: :TCP, tcp: :TCP, udp: :UDP }.map do |type, prot|
       addr = Addr[settings.host, settings.send(type), prot]
@@ -19,32 +19,32 @@ module Kamerling class ServerRunner
     self
   end
 
-  attr_reader :args, :servers
-  private     :args, :servers
+  attr_reader :servers
+  private     :servers
 
   private
 
-  Settings = Struct.new(*%i(db host http tcp udp))
+  Settings = Struct.new(*%i(db host http tcp udp)) do
+    def initialize args
+      s = defaults
+      OptionParser.new do |opt|
+        opt.on("--db #{s[:db]}", String, 'database') { |db|   s[:db]   = db   }
+        opt.on("--host #{s[:host]}", String, 'host') { |host| s[:host] = host }
+        opt.on('--http 0', Integer, 'HTTP port')     { |http| s[:http] = http }
+        opt.on('--tcp 0',  Integer, 'TCP port')      { |tcp|  s[:tcp]  = tcp  }
+        opt.on('--udp 0',  Integer, 'UDP port')      { |udp|  s[:udp]  = udp  }
+      end.parse! args
+      super(*self.class.members.map { |key| s[key] })
+    end
+
+    private
+
+    def defaults
+      { db: 'sqlite::memory:', host: '127.0.0.1' }
+    end
+  end
 
   def def_classes
     { http: Server::HTTP, tcp: Server::TCP, udp: Server::UDP }
-  end
-
-  def defaults
-    Settings.new 'sqlite::memory:', '127.0.0.1'
-  end
-
-  def parse_into set
-    OptionParser.new do |opt|
-      opt.on("--db #{set.db}", String, 'database') { |db|   set.db   = db   }
-      opt.on("--host #{set.host}", String, 'host') { |host| set.host = host }
-      opt.on('--http 0', Integer, 'HTTP port')     { |http| set.http = http }
-      opt.on('--tcp 0',  Integer, 'TCP port')      { |tcp|  set.tcp  = tcp  }
-      opt.on('--udp 0',  Integer, 'UDP port')      { |udp|  set.udp  = udp  }
-    end.parse! args
-  end
-
-  def settings
-    @settings ||= defaults.tap { |set| parse_into set }
   end
 end end
