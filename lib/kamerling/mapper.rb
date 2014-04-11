df498652a39a3866d214c8208583bfcbbdae20bf
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-04-11 08:25:33 +0200

    turn Mapper into a module

--- lib/kamerling/mapper.rb
+++ lib/kamerling/mapper.rb
@@ -1,10 +1,12 @@
-module Kamerling class Mapper
-  def self.from_h klass, hash
+module Kamerling module Mapper
+  module_function
+
+  def from_h klass, hash
     hash = hash.merge addr: Addr[hash[:host], hash[:port], hash[:prot]]
     klass.new hash
   end
 
-  def self.to_h object
+  def to_h object
     object.to_h.reduce({}) do |hash, (key, value)|
       hash.merge case key
                  when :addr    then value.to_h
