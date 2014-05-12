017ec329d7903f776c1a8f5d0a536272488a671d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-05-12 08:12:23 +0200

    factor out HTTPAPI#render_template

--- lib/kamerling/http_api.rb
+++ lib/kamerling/http_api.rb
@@ -13,27 +13,33 @@ module Kamerling class HTTPAPI < Sinatra::Base
   configure { set repos: Repos }
 
   get '/' do
-    slim :root
+    render_template :root
   end
 
   get '/clients' do
-    warn_off { slim :clients, locals: { clients: repos.clients } }
+    render_template :clients, locals: { clients: repos.clients }
   end
 
   get '/projects' do
-    warn_off { slim :projects, locals: { projects: repos.projects } }
+    render_template :projects, locals: { projects: repos.projects }
   end
 
   get '/projects/:project_uuid' do
     project = repos.project params['project_uuid']
     clients = repos.clients_for project
     tasks   = repos.tasks_for project
-    warn_off { slim :project, locals: { clients: clients, tasks: tasks } }
+    render_template :project, locals: { clients: clients, tasks: tasks }
   end
 
   post '/projects' do
     uuid = params.fetch('uuid') { UUID.new }
     repos << Project.new(name: params['name'], uuid: uuid)
     redirect '/projects'
+  end
+
+  private
+
+  def render_template template, locals: {}
+    warn_off { slim template, locals: locals }
   end
 end end
