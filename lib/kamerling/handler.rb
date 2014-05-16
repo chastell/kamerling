39972a13bc22a5ecea49e5776c21363d1cdf4c05
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-05-17 00:43:01 +0200

    inline Handler#process

--- lib/kamerling/handler.rb
+++ lib/kamerling/handler.rb
@@ -7,18 +7,12 @@ module Kamerling class Handler
   end
 
   def handle message, addr
-    process message, addr
-  end
-
-  attr_reader :receiver, :registrar
-  private     :receiver, :registrar
-
-  private
-
-  def process message, addr
     case message.type
     when :RGST then registrar.register addr: addr, message: message
     when :RSLT then receiver.receive   addr: addr, message: message
     end
   end
+
+  attr_reader :receiver, :registrar
+  private     :receiver, :registrar
 end end
