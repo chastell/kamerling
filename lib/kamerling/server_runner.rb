1dcdff839c314380b273c9621e3305d0cc590f01
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-05-19 00:09:03 +0200

    ServerRunner: use Settings#server_addrs

--- lib/kamerling/server_runner.rb
+++ lib/kamerling/server_runner.rb
@@ -10,10 +10,9 @@ module Kamerling class ServerRunner
   def initialize args, classes: def_classes, orm: Sequel, repos: Repos
     settings = Settings.new args
     repos.db = orm.connect settings.db
-    @servers = { http: :TCP, tcp: :TCP, udp: :UDP }.map do |type, prot|
-      addr = Addr[settings.host, settings.send(type), prot]
-      classes[type].new addr: addr if addr.port
-    end.compact
+    @servers = settings.server_addrs.map do |type, addr|
+      classes[type].new addr: addr
+    end
   end
 
   def join
