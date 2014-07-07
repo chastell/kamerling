cbdb95963121308b6ef67791f965bb424f9d5a46
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-07-08 00:46:38 +0200

    simplify Client spec

--- spec/kamerling/client_spec.rb
+++ spec/kamerling/client_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/kamerling/client'
 module Kamerling describe Client do
   describe '#busy' do
     it 'defaults to false' do
-      Client.new(addr: fake(:addr)).busy.must_equal false
+      refute Client.new.busy
     end
   end
 end end
