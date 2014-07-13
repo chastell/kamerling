8d3c757188ac22c97f77de24a6e89e57e119a992
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-07-13 21:40:31 +0200

    drop Message.[]

--- lib/kamerling/message.rb
+++ lib/kamerling/message.rb
@@ -3,12 +3,6 @@ require_relative 'uuid'
 module Kamerling class Message
   UnknownType = Class.new RuntimeError
 
-  def self.[] client: req(:client), payload: req(:payload),
-              project: req(:project), task: req(:task), type: req(:type)
-    new raw: "#{type}\0\0\0\0\0\0\0\0\0\0\0\0" + UUID.bin(client.uuid) +
-      UUID.bin(project.uuid) + UUID.bin(task.uuid) + payload
-  end
-
   def self.parse raw
     new raw: raw
   end
