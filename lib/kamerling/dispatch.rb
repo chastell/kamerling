d5a1cfb30655e390187d7bc2effb27b66f427828
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-07-14 22:02:57 +0200

    add Dispatch

---
+++ lib/kamerling/dispatch.rb
@@ -0,0 +1,11 @@
+require_relative 'addr'
+require_relative 'client'
+require_relative 'project'
+require_relative 'task'
+require_relative 'uuid_entity'
+
+module Kamerling class Dispatch < UUIDEntity
+  attrs addr: Addr, client: Client, dispatched_at: Time, project: Project,
+        task: Task
+  defaults dispatched_at: -> * { Time.now }
+end end
---
+++ spec/kamerling/dispatch_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../spec_helper'
+require_relative '../../lib/kamerling/dispatch'
+
+module Kamerling describe Dispatch do
+  describe '#dispatched_at' do
+    it 'defaults to the current time' do
+      assert Dispatch.new.dispatched_at.between? Time.now - 1, Time.now + 1
+    end
+
+    it 'defaults to the time of Dispatch’s creation' do
+      Dispatch.new.dispatched_at.wont_equal Dispatch.new.dispatched_at
+    end
+  end
+end end
