af9c6a2ae74127d1413dfee962974842e5429d6c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2014-11-09 22:18:25 +0100

    Registrar: pass everything to the constructor

--- lib/kamerling/registrar.rb
+++ lib/kamerling/registrar.rb
@@ -7,22 +7,22 @@ require_relative 'uuid'
 module Kamerling
   class Registrar
     def self.register(addr:, message:, repos: Repos)
-      new(repos: repos).register addr: addr, message: message
+      new(addr: addr, message: message, repos: repos).register
     end
 
-    def initialize(repos: Repos)
-      @repos = repos
+    def initialize(addr:, message:, repos: Repos)
+      @addr, @message, @repos = addr, message, repos
     end
 
-    def register(addr:, message:)
+    def register
       client  = find_or_create_client(addr: addr, uuid: message.client_uuid)
       project = repos[Project][message.project_uuid]
       repos << client
       repos << Registration.new(addr: addr, client: client, project: project)
     end
 
-    attr_reader :repos
-    private     :repos
+    attr_reader :addr, :message, :repos
+    private     :addr, :message, :repos
 
     private
 
