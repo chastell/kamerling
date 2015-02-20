a1424b6ad5b08144ccded70ef5ef3c7d6618e253
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-02-21 00:19:27 +0100

    switch to private_attr/everywhere

--- lib/kamerling.rb
+++ lib/kamerling.rb
@@ -1,6 +1,4 @@
-require 'private_attr'
-Class.include PrivateAttr
-
+require 'private_attr/everywhere'
 require_relative 'kamerling/core_extensions/main'
 require_relative 'kamerling/logging'
 require_relative 'kamerling/server_runner'
