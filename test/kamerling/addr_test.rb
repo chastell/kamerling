a569ffb70b945472b1a5304dcf0866284c29da2b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-07-25 00:11:19 +0200

    port Addr test to Expectation syntax

--- test/kamerling/addr_test.rb
+++ test/kamerling/addr_test.rb
@@ -9,34 +9,34 @@ module Kamerling
     describe '#connectable?' do
       it 'is a predicate whether the (TCP) address is connectable' do
         server = TCPServer.new(*addr)
-        addr.must_be :connectable?
+        assert addr.connectable?
         server.close
-        addr.wont_be :connectable?
+        refute addr.connectable?
       end
     end
 
     describe '#to_a' do
       it 'returns host + port for splat use' do
         splat = *addr
-        splat.must_equal ['localhost', 1981]
+        _(splat).must_equal ['localhost', 1981]
       end
     end
 
     describe '#to_h' do
       it 'returns a Hash with Integer and String values' do
-        addr.to_h.must_equal host: 'localhost', port: 1981, prot: 'TCP'
+        _(addr.to_h).must_equal host: 'localhost', port: 1981, prot: 'TCP'
       end
     end
 
     describe '#to_s' do
       it 'returns the Addr in URI notation' do
-        addr.to_s.must_equal 'tcp://localhost:1981'
+        _(addr.to_s).must_equal 'tcp://localhost:1981'
       end
     end
 
     describe '#uri' do
       it 'returns and URI representation of the Addr' do
-        addr.uri.must_equal URI.parse('tcp://localhost:1981')
+        _(addr.uri).must_equal URI.parse('tcp://localhost:1981')
       end
     end
   end
