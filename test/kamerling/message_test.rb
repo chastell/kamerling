62e91e9da47b385414e0665880d24ce23cc67fbf
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-08-02 00:18:09 +0200

    port Message spec to Expectation syntax

--- test/kamerling/message_test.rb
+++ test/kamerling/message_test.rb
@@ -19,11 +19,11 @@ module Kamerling                          # rubocop:disable Metrics/ModuleLength
         task    = Task.new
         message = Message.build(client: client, payload: 'pay',
                                 project: project, task: task, type: :DATA)
-        message.client_uuid.must_equal client.uuid
-        message.project_uuid.must_equal project.uuid
-        message.task_uuid.must_equal task.uuid
-        message.payload.must_equal 'pay'
-        message.type.must_equal :DATA
+        _(message.client_uuid).must_equal client.uuid
+        _(message.project_uuid).must_equal project.uuid
+        _(message.task_uuid).must_equal task.uuid
+        _(message.payload).must_equal 'pay'
+        _(message.type).must_equal :DATA
       end
     end
 
@@ -33,17 +33,17 @@ module Kamerling                          # rubocop:disable Metrics/ModuleLength
         project = Project.new
         task    = Task.new(data: 'pay')
         message = Message.data(client: client, project: project, task: task)
-        message.client_uuid.must_equal client.uuid
-        message.project_uuid.must_equal project.uuid
-        message.task_uuid.must_equal task.uuid
-        message.payload.must_equal 'pay'
-        message.type.must_equal :DATA
+        _(message.client_uuid).must_equal client.uuid
+        _(message.project_uuid).must_equal project.uuid
+        _(message.task_uuid).must_equal task.uuid
+        _(message.payload).must_equal 'pay'
+        _(message.type).must_equal :DATA
       end
     end
 
     describe '.parse' do
       it 'raises on unknown message types' do
-        -> { Message.parse('MESS age') }.must_raise Message::UnknownType
+        _(-> { Message.parse('MESS age') }).must_raise Message::UnknownType
       end
 
       it 'doesn’t raise on empty messages' do
@@ -56,9 +56,9 @@ module Kamerling                          # rubocop:disable Metrics/ModuleLength
         client  = Client.new
         project = Project.new
         message = Message.rgst(client: client, project: project)
-        message.client_uuid.must_equal client.uuid
-        message.project_uuid.must_equal project.uuid
-        message.type.must_equal :RGST
+        _(message.client_uuid).must_equal client.uuid
+        _(message.project_uuid).must_equal project.uuid
+        _(message.type).must_equal :RGST
       end
     end
 
@@ -67,44 +67,44 @@ module Kamerling                          # rubocop:disable Metrics/ModuleLength
         client  = Client.new
         task    = Task.new(project: Project.new)
         message = Message.rslt(client: client, payload: 'data', task: task)
-        message.client_uuid.must_equal client.uuid
-        message.payload.must_equal 'data'
-        message.task_uuid.must_equal task.uuid
-        message.type.must_equal :RSLT
+        _(message.client_uuid).must_equal client.uuid
+        _(message.payload).must_equal 'data'
+        _(message.task_uuid).must_equal task.uuid
+        _(message.type).must_equal :RSLT
       end
     end
 
     describe '#client_type' do
       it 'returns the client’s type' do
-        mess.client_type.must_equal :"\0\0\0\0"
+        _(mess.client_type).must_equal :"\0\0\0\0"
         fpga_mess = Message.parse("RGSTFPGA\0\0\0\0\0\0\0\0" \
                                   '16B client  UUID16B project UUID' \
                                   '16B task    UUID')
-        fpga_mess.client_type.must_equal :FPGA
+        _(fpga_mess.client_type).must_equal :FPGA
       end
     end
 
     describe '#client_uuid' do
       it 'returns the client UUID' do
-        mess.client_uuid.must_equal '31364220-636c-6965-6e74-202055554944'
+        _(mess.client_uuid).must_equal '31364220-636c-6965-6e74-202055554944'
       end
     end
 
     describe '#payload' do
       it 'returns the result payload' do
-        mess.payload.must_equal 'some payload'
+        _(mess.payload).must_equal 'some payload'
       end
     end
 
     describe '#project_uuid' do
       it 'returns the project UUID' do
-        mess.project_uuid.must_equal '31364220-7072-6f6a-6563-742055554944'
+        _(mess.project_uuid).must_equal '31364220-7072-6f6a-6563-742055554944'
       end
     end
 
     describe '#task_uuid' do
       it 'returns the task UUID' do
-        mess.task_uuid.must_equal '31364220-7461-736b-2020-202055554944'
+        _(mess.task_uuid).must_equal '31364220-7461-736b-2020-202055554944'
       end
     end
 
@@ -117,14 +117,14 @@ module Kamerling                          # rubocop:disable Metrics/ModuleLength
 
     describe '#to_s' do
       it 'returns the raw bytes' do
-        mess.to_s.must_equal "#{mess.type}\0\0\0\0\0\0\0\0\0\0\0\0" \
+        _(mess.to_s).must_equal "#{mess.type}\0\0\0\0\0\0\0\0\0\0\0\0" \
           '16B client  UUID16B project UUID16B task    UUIDsome payload'
       end
     end
 
     describe '#type' do
       it 'returns the message type' do
-        mess.type.must_match(/\A[A-Z]{4}\z/)
+        _(mess.type).must_match(/\A[A-Z]{4}\z/)
       end
     end
   end
