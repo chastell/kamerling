fea2d7b303ad798e50f631c6af8ec8c29e453b9f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-08-05 06:05:07 +0200

    port Registrar test to Expectation syntax

--- test/kamerling/registrar_test.rb
+++ test/kamerling/registrar_test.rb
@@ -26,19 +26,19 @@ module Kamerling
 
       it 'registers that the given client can do the given project' do
         Registrar.register addr: addr, message: mess, repos: repos
-        repos.must_have_received :<<, [any(Registration)]
+        _(repos).must_have_received :<<, [any(Registration)]
       end
 
       it 'updates the clien’t addr' do
         Registrar.register addr: addr, message: mess, repos: repos
-        repos.must_have_received :<<, [client]
+        _(repos).must_have_received :<<, [client]
       end
 
       it 'doesn’t blow up when a new client tries to register' do
         empty_repo = fake(:repo, :[] => -> { fail Repo::NotFound })
         stub(repos).[](Client) { empty_repo }
         Registrar.register addr: addr, message: mess, repos: repos
-        repos.must_have_received :<<, [client]
+        _(repos).must_have_received :<<, [client]
       end
     end
   end
