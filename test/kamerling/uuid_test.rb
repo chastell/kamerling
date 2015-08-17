d4458069b88809e000e1e17d4badda5124c43abb
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-08-17 10:15:03 +0200

    port UUID test to Expectation syntax

--- test/kamerling/uuid_test.rb
+++ test/kamerling/uuid_test.rb
@@ -6,21 +6,21 @@ module Kamerling
     describe '.[]' do
       it 'creates a normal UUID representation from binary data' do
         uuid = '31364220-7072-6f6a-6563-742055554944'
-        UUID['16B project UUID'].must_equal uuid
+        _(UUID['16B project UUID']).must_equal uuid
       end
     end
 
     describe '.bin' do
       it 'returns the binary representation of an UUID' do
-        UUID.bin('31364220-7072-6f6a-6563-742055554944')
-          .must_equal '16B project UUID'
+        uuid = UUID.bin('31364220-7072-6f6a-6563-742055554944')
+        _(uuid).must_equal '16B project UUID'
       end
     end
 
     describe '.new' do
       it 'creates a new random UUID' do
-        UUID.new.must_match(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
-        UUID.new.wont_equal UUID.new
+        _(UUID.new).must_match(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
+        _(UUID.new).wont_equal UUID.new
       end
     end
   end
