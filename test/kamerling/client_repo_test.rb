508eb270719d47447de8d8cc04a90fa7ce9e95ce
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-09-20 00:47:49 +0200

    ClientRepo test: rename client to entity and factor out table

--- test/kamerling/client_repo_test.rb
+++ test/kamerling/client_repo_test.rb
@@ -7,16 +7,17 @@ module Kamerling
   describe ClientRepo do
     Sequel.extension :migration
 
-    let(:client) do
+    let(:db) { Sequel.sqlite }
+    let(:entity) do
       addr = Addr['localhost', 1981, :TCP]
       Client.new(addr: addr, busy: true, type: :FPGA, uuid: 'an UUID')
     end
-    let(:db)   { Sequel.sqlite      }
     let(:repo) { ClientRepo.new(db) }
     let(:row) do
       { busy: true, host: 'localhost', port: 1981, prot: 'TCP', type: 'FPGA',
         uuid: 'an UUID' }
     end
+    let(:table) { db[:clients] }
 
     before do
       path = "#{__dir__}/../../lib/kamerling/migrations"
@@ -25,25 +26,25 @@ module Kamerling
 
     describe '#<<' do
       it 'adds a new Client to the repo' do
-        assert db[:clients].empty?
-        repo << client
-        _(db[:clients].first).must_equal row
+        assert table.empty?
+        repo << entity
+        _(table.first).must_equal row
       end
 
       it 'updates the row if the Client exists' do
-        db[:clients].insert(uuid: 'an UUID', busy: false, host: '127.0.0.1',
-                            port: 1979, prot: 'UDP')
-        repo << client
-        _(db[:clients].first).must_equal row
+        table.insert(uuid: 'an UUID', busy: false, host: '127.0.0.1',
+                     port: 1979, prot: 'UDP')
+        repo << entity
+        _(table.first).must_equal row
       end
     end
 
     describe '#all' do
       it 'returns all the rows as Clients' do
-        db[:clients].insert(uuid: 'UDP client', busy: false, host: '127.0.0.1',
-                            port: 1979, prot: 'UDP')
-        db[:clients].insert(uuid: 'TCP client', busy: true, host: 'localhost',
-                            port: 1981, prot: 'TCP')
+        table.insert(uuid: 'UDP client', busy: false, host: '127.0.0.1',
+                     port: 1979, prot: 'UDP')
+        table.insert(uuid: 'TCP client', busy: true, host: 'localhost',
+                     port: 1981, prot: 'TCP')
         _(repo.all).must_equal [
           Client.new(uuid: 'UDP client'),
           Client.new(uuid: 'TCP client'),
@@ -53,9 +54,9 @@ module Kamerling
 
     describe '#fetch' do
       it 'returns the Client with the given UUID' do
-        db[:clients].insert(uuid: 'an UUID', busy: false, host: '127.0.0.1',
-                            port: 1979, prot: 'UDP')
-        _(repo.fetch('an UUID')).must_equal client
+        table.insert(uuid: 'an UUID', busy: false, host: '127.0.0.1',
+                     port: 1979, prot: 'UDP')
+        _(repo.fetch('an UUID')).must_equal entity
       end
 
       it 'evaluates the block if the given UUID is missing' do
