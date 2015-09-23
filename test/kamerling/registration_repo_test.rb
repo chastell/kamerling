c040b8c50e51e170fa30e67b3f4bf6870b6046b1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-09-23 06:21:11 +0200

    refactor RegistrationRepo test

--- test/kamerling/registration_repo_test.rb
+++ test/kamerling/registration_repo_test.rb
@@ -10,27 +10,34 @@ module Kamerling
   describe RegistrationRepo do
     Sequel.extension :migration
 
+    let(:addr) { Addr['localhost', 1981, :TCP] }
+    let(:client) do
+      Client.new(addr: addr, busy: true, type: :FPGA, uuid: 'cUUID')
+    end
+    let(:db) { Sequel.sqlite }
+    let(:entity) do
+      Registration.new(addr: addr, client: client, project: project,
+                       uuid: 'rUUID')
+    end
+    let(:project) { Project.new(name: 'GIMPS', uuid: 'pUUID') }
+    let(:repo) { RegistrationRepo.new(db) }
+    let(:table) { db[:registrations] }
+
+    before do
+      path = "#{__dir__}/../../lib/kamerling/migrations"
+      Sequel::Migrator.run db, path
+    end
+
     describe '#<<' do
       it 'adds a new Registration to the repo' do
-        db = Sequel.sqlite
-        path = "#{__dir__}/../../lib/kamerling/migrations"
-        Sequel::Migrator.run db, path
-        repo = RegistrationRepo.new(db)
-        assert db[:registrations].empty?
-        addr    = Addr['localhost', 1981, :TCP]
-        client  = Client.new(addr: addr, busy: true, type: :FPGA, uuid: 'cUUID')
-        project = Project.new(name: 'GIMPS', uuid: 'pUUID')
+        assert table.empty?
         db[:clients] << Mapper.to_h(client)
         db[:projects] << Mapper.to_h(project)
-        registration = Registration.new(addr: addr, client: client,
-                                        project: project, uuid: 'rUUID')
-        repo << registration
-        _(db[:registrations].first).must_equal client_uuid: 'cUUID',
-                                               host: 'localhost', port: 1981,
-                                               project_uuid: 'pUUID',
-                                               prot: 'TCP',
-                                               registered_at: any(Time),
-                                               uuid: 'rUUID'
+        repo << entity
+        _(table.first).must_equal client_uuid: 'cUUID', host: 'localhost',
+                                  port: 1981, project_uuid: 'pUUID',
+                                  prot: 'TCP', registered_at: any(Time),
+                                  uuid: 'rUUID'
       end
     end
   end
