5d401ffdb3cc5d1a2c43b4026981516d3145733e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-10-04 10:54:01 +0200

    ClientRepo test: make the lets readable

--- test/kamerling/client_repo_test.rb
+++ test/kamerling/client_repo_test.rb
@@ -10,17 +10,19 @@ module Kamerling
 
     Sequel.extension :migration
 
-    let(:db) { Sequel.sqlite }
+    let(:db)    { Sequel.sqlite      }
+    let(:repo)  { ClientRepo.new(db) }
+    let(:table) { db[:clients]       }
+
     let(:entity) do
       addr = Addr['localhost', 1981, :TCP]
       Client.new(addr: addr, busy: true, type: :FPGA, uuid: 'an UUID')
     end
-    let(:repo) { ClientRepo.new(db) }
+
     let(:row) do
       { busy: true, host: 'localhost', port: 1981, prot: 'TCP', type: 'FPGA',
         uuid: 'an UUID' }
     end
-    let(:table) { db[:clients] }
 
     before do
       path = "#{__dir__}/../../lib/kamerling/migrations"
