52ad0d773c936ffca0a06859b65c6dee07c8493f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-10-19 00:48:16 +0200

    add Client#new_to_h

--- lib/kamerling/client.rb
+++ lib/kamerling/client.rb
@@ -6,6 +6,10 @@ module Kamerling
     attrs addr: Addr, busy: Boolean, type: Symbol
     defaults busy: false
 
+    def new_to_h
+      addr.to_h.merge(busy: busy, type: type.to_s, uuid: uuid)
+    end
+
     def to_h
       attributes.merge(type: type.to_s)
     end
--- test/kamerling/client_test.rb
+++ test/kamerling/client_test.rb
@@ -9,5 +9,15 @@ module Kamerling
         refute Client.new.busy
       end
     end
+
+    describe '#new_to_h' do
+      it 'returns a Hash representation of the Client' do
+        addr   = Addr['127.0.0.1', 1979, :UDP]
+        client = Client.new(addr: addr, busy: true, type: :FPGA)
+        _(client.new_to_h).must_equal busy: true, host: '127.0.0.1', port: 1979,
+                                      prot: 'UDP', type: 'FPGA',
+                                      uuid: any(String)
+      end
+    end
   end
 end
