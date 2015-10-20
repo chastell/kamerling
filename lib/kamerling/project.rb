0dce60b8489e39dfdd9289c9b390ca998da0884f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-10-20 17:42:50 +0200

    add Project#new_to_h

--- lib/kamerling/project.rb
+++ lib/kamerling/project.rb
@@ -3,5 +3,7 @@ require_relative 'entity'
 module Kamerling
   class Project < Entity
     attrs name: String
+
+    alias_method :new_to_h, :to_h
   end
 end
---
+++ test/kamerling/project_test.rb
@@ -0,0 +1,13 @@
+require_relative '../test_helper'
+require_relative '../../lib/kamerling/project'
+
+module Kamerling
+  describe Project do
+    describe '#new_to_h' do
+      it 'returns a Hash representation of the Project' do
+        gimps = Project.new(name: 'GIMPS')
+        _(gimps.new_to_h).must_equal name: 'GIMPS', uuid: any(String)
+      end
+    end
+  end
+end
