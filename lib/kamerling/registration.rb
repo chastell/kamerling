ea740d72ea54d1722d788549f87f3dae213f3d66
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-10-22 00:29:00 +0200

    add Registration#new_to_h

--- lib/kamerling/registration.rb
+++ lib/kamerling/registration.rb
@@ -7,5 +7,10 @@ module Kamerling
   class Registration < Entity
     attrs addr: Addr, client: Client, project: Project, registered_at: Time
     defaults registered_at: -> (*) { Time.now.utc }
+
+    def new_to_h
+      addr.to_h.merge(client_uuid: client.uuid, project_uuid: project.uuid,
+                      registered_at: registered_at.iso8601, uuid: uuid)
+    end
   end
 end
