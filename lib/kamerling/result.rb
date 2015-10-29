3408d9ee8cb6e57f29572cd4a304a681e22c02f7
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-10-29 01:26:58 +0100

    add Task#new_to_h

--- lib/kamerling/result.rb
+++ lib/kamerling/result.rb
@@ -8,5 +8,11 @@ module Kamerling
     attrs addr: Addr, client: Client, data: String, received_at: Time,
           task: Task
     defaults received_at: -> (*) { Time.now.utc }
+
+    def new_to_h
+      addr.to_h.merge(client_uuid: client.uuid, data: data,
+                      received_at: received_at.iso8601, task_uuid: task.uuid,
+                      uuid: uuid)
+    end
   end
 end
--- test/kamerling/result_test.rb
+++ test/kamerling/result_test.rb
@@ -3,6 +3,22 @@ require_relative '../../lib/kamerling/result'
 
 module Kamerling
   describe Result do
+    describe '#new_to_h' do
+      it 'returns a Hash representation of the Registration' do
+        addr    = Addr['127.0.0.1', 1979, :UDP]
+        client  = Client.new(uuid: 'client UUID')
+        task    = Task.new(uuid: 'task UUID')
+        result  = Result.new(addr: addr, client: client, data: 'data',
+                             task: task)
+        iso8601 = ->(time) { time[/\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z\z/] }
+        _(result.new_to_h).must_equal client_uuid: 'client UUID', data: 'data',
+                                      host: '127.0.0.1', port: 1979,
+                                      task_uuid: 'task UUID', prot: 'UDP',
+                                      received_at: matches(&iso8601),
+                                      uuid: any(String)
+      end
+    end
+
     describe '#received_at' do
       it 'defaults to the current time' do
         assert Result.new.received_at.between?(Time.now - 1, Time.now + 1)
