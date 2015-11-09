7600897811ef33becdb2941def60fde0b22e5b47
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2015-11-09 08:44:06 +0000

    add Project#tasks

--- lib/kamerling/project.rb
+++ lib/kamerling/project.rb
@@ -2,10 +2,10 @@ require_relative 'entity'
 
 module Kamerling
   class Project < Entity
-    attrs clients: Array, name: String
+    attrs clients: Array, name: String, tasks: Array
 
     def new_to_h
-      to_h.reject { |key, _| key == :clients }
+      to_h.reject { |key, _| key == :clients or key == :tasks }
     end
   end
 end
