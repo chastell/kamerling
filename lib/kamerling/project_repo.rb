f2bade0050fb6d894ffe67f708431cdf5a7fd020
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-03-18 00:07:07 +0100

    drop ProjectRepo#fetch_with_clients_and_tasks

--- lib/kamerling/project_repo.rb
+++ lib/kamerling/project_repo.rb
@@ -8,28 +8,8 @@ require_relative 'task'
 module Kamerling
   class ProjectRepo < NewRepo
     def initialize(db = Settings.new.db_conn)
-      @db    = db
       @klass = Project
       @table = db[:projects]
-    end
-
-    def fetch_with_clients_and_tasks(uuid)
-      clients = client_hashes(uuid).map(&Client.method(:new))
-      tasks   = task_hashes(uuid).map(&Task.method(:new))
-      klass.new(table[uuid: uuid].merge(clients: clients, tasks: tasks))
-    end
-
-    private
-
-    attr_reader :db, :table
-
-    def client_hashes(uuid)
-      db[:registrations].join(:clients, uuid: :client_uuid)
-                        .where(project_uuid: uuid).all
-    end
-
-    def task_hashes(uuid)
-      db[:tasks].where(project_uuid: uuid).all
     end
   end
 end
