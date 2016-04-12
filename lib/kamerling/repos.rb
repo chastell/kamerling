a232a40ade091d8f5db5de1ea02b38ee0c418d5c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-04-12 06:15:32 +0200

    Repos: drop state

--- lib/kamerling/repos.rb
+++ lib/kamerling/repos.rb
@@ -1,33 +1,17 @@
 # frozen_string_literal: true
 
-require 'sequel'
-require_relative 'client'
 require_relative 'client_repo'
 require_relative 'dispatch_repo'
-require_relative 'project'
 require_relative 'project_repo'
-require_relative 'registration'
 require_relative 'registration_repo'
-require_relative 'repo'
 require_relative 'result_repo'
-require_relative 'task'
 require_relative 'task_repo'
-
-Sequel.extension :migration
 
 module Kamerling
   class Repos
     class << self
-      attr_writer :repos
-
       def client_repo
         @client_repo ||= ClientRepo.new
-      end
-
-      def db=(db)
-        Sequel::Migrator.run db, "#{__dir__}/migrations"
-        @repos = nil
-        @db    = db
       end
 
       def dispatch_repo
@@ -48,19 +32,6 @@ module Kamerling
 
       def task_repo
         @task_repo ||= TaskRepo.new
-      end
-
-      private
-
-      def db
-        @db ||= self.db = Sequel.sqlite
-      end
-
-      def repos
-        @repos ||= Hash.new do |repos, klass|
-          table = "#{klass.name.split('::').last.downcase}s".to_sym
-          repos[klass] = Repo.new(klass, db[table])
-        end
       end
     end
   end
--- test/kamerling/repos_test.rb
+++ test/kamerling/repos_test.rb
@@ -1,37 +1,19 @@
 # frozen_string_literal: true
 
-require 'sequel'
 require_relative '../test_helper'
-require_relative '../../lib/kamerling/addr'
-require_relative '../../lib/kamerling/client'
 require_relative '../../lib/kamerling/client_repo'
 require_relative '../../lib/kamerling/dispatch_repo'
-require_relative '../../lib/kamerling/project'
 require_relative '../../lib/kamerling/project_repo'
-require_relative '../../lib/kamerling/registration'
 require_relative '../../lib/kamerling/registration_repo'
-require_relative '../../lib/kamerling/repo'
 require_relative '../../lib/kamerling/repos'
-require_relative '../../lib/kamerling/result'
 require_relative '../../lib/kamerling/result_repo'
-require_relative '../../lib/kamerling/task'
 require_relative '../../lib/kamerling/task_repo'
-require_relative '../../lib/kamerling/uuid'
 
 module Kamerling
   describe Repos do
     describe '.client_repo' do
       it 'returns a ClientRepo' do
         _(Repos.client_repo).must_be_kind_of ClientRepo
-      end
-    end
-
-    describe '.db=' do
-      it 'auto-migrates the passed db' do
-        db = Sequel.sqlite
-        _(db.tables).wont_include :schema_info
-        Repos.db = db
-        _(db.tables).must_include :schema_info
       end
     end
 
