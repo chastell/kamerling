6fe06627b22778f1c6403539f2e4bb6e34b0d5ba
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-04-19 00:04:06 +0200

    drop Client#to_h

--- lib/kamerling/client.rb
+++ lib/kamerling/client.rb
@@ -7,9 +7,5 @@ module Kamerling
   class Client < Entity
     attrs addr: Addr, busy: Boolean, type: Symbol
     defaults busy: false
-
-    def to_h
-      addr.to_h.merge(busy: busy, type: type.to_s, uuid: uuid)
-    end
   end
 end
