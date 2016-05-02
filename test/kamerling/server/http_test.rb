05cc61d396d536494db2fbe073f9595d62d25c63
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-05-02 07:59:25 +0200

    test Server::HTTP#==

--- test/kamerling/server/http_test.rb
+++ test/kamerling/server/http_test.rb
@@ -8,17 +8,25 @@ require_relative '../../../lib/kamerling/server/http'
 module Kamerling
   describe Server::HTTP do
     let(:addr) { Addr['localhost', 2009, :TCP] }
+    let(:http) { Server::HTTP.new(addr: addr)  }
+
+    describe '#==' do
+      it 'compares servers by their addresses' do
+        assert http == Server::HTTP.new(addr: Addr['localhost', 2009, :TCP])
+        refute http == Server::HTTP.new(addr: Addr['localhost', 2010, :TCP])
+      end
+    end
 
     describe '#addr' do
       it 'returns the server’s host + port as a TCP addr' do
-        _(Server::HTTP.new(addr: addr).addr).must_equal addr
+        _(http.addr).must_equal addr
       end
     end
 
     describe '#start, #stop' do
       it 'starts/stops a HTTP server on the given host and port' do
         capture_io do
-          server = Server::HTTP.new(addr: addr).start
+          server = http.start
           uri = URI.parse('http://localhost:2009')
           _(Net::HTTP.get_response(uri)).must_be_kind_of Net::HTTPSuccess
           server.stop
