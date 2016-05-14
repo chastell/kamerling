4bad7d18c6c6d5d6bf21968f35d92753fe78b330
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-05-14 10:52:56 +0200

    ServerRunner: use Settings#servers

--- lib/kamerling/server_runner.rb
+++ lib/kamerling/server_runner.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-require_relative 'addr'
-require_relative 'server/http'
-require_relative 'server/tcp'
-require_relative 'server/udp'
 require_relative 'settings'
 
 module Kamerling
   class ServerRunner
-    def initialize(settings, classes: def_classes)
-      @servers = settings.server_addrs.map do |type, addr|
-        classes[type].new(addr: addr)
-      end
+    def initialize(settings)
+      @servers = settings.servers
     end
 
     def join
@@ -26,9 +20,5 @@ module Kamerling
     private
 
     attr_reader :servers
-
-    def def_classes
-      { http: Server::HTTP, tcp: Server::TCP, udp: Server::UDP }
-    end
   end
 end
--- test/kamerling/server_runner_test.rb
+++ test/kamerling/server_runner_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
-require_relative '../../lib/kamerling/addr'
 require_relative '../../lib/kamerling/server/http'
 require_relative '../../lib/kamerling/server/tcp'
 require_relative '../../lib/kamerling/server/udp'
@@ -10,20 +9,16 @@ require_relative '../../lib/kamerling/settings'
 
 module Kamerling
   describe ServerRunner do
-    let(:http)     { fake { Server::HTTP }                                  }
-    let(:tcp)      { fake { Server::TCP  }                                  }
-    let(:udp)      { fake { Server::UDP  }                                  }
-    let(:http_cl)  { fake(Server::HTTP, as: :class, new: http)              }
-    let(:tcp_cl)   { fake(Server::TCP,  as: :class, new: tcp)               }
-    let(:udp_cl)   { fake(Server::UDP,  as: :class, new: udp)               }
-    let(:classes)  { { http: http_cl, tcp: tcp_cl, udp: udp_cl }            }
-    let(:addrs)    { { http: fake(Addr), tcp: fake(Addr), udp: fake(Addr) } }
-    let(:settings) { fake(Settings, server_addrs: addrs)                    }
+    let(:http) { fake { Server::HTTP } }
+    let(:tcp)  { fake { Server::TCP  } }
+    let(:udp)  { fake { Server::UDP  } }
+
+    let(:settings) { fake(Settings, servers: [http, tcp, udp]) }
 
     describe '#join' do
       it 'joins all the created servers' do
-        stub(settings).server_addrs { { http: Addr['0.0.0.0', 1234, :TCP] } }
-        ServerRunner.new(settings, classes: classes).join
+        stub(settings).servers { [http] }
+        ServerRunner.new(settings).join
         http.must_have_received :join, []
         tcp.wont_have_received :join,  []
         udp.wont_have_received :join,  []
@@ -32,22 +27,22 @@ module Kamerling
 
     describe '#start' do
       it 'starts the servers based on the given command-line parameters' do
-        ServerRunner.new(settings, classes: classes).start
+        ServerRunner.new(settings).start
         http.must_have_received :start, []
         tcp.must_have_received :start,  []
         udp.must_have_received :start,  []
       end
 
       it 'starts only the servers for which the port was given' do
-        stub(settings).server_addrs { { http: fake(Addr) } }
-        ServerRunner.new(settings, classes: classes).start
+        stub(settings).servers { [http] }
+        ServerRunner.new(settings).start
         http.must_have_received :start, []
         tcp.wont_have_received :start,  []
         udp.wont_have_received :start,  []
       end
 
       it 'returns self' do
-        server_runner = ServerRunner.new(settings, classes: classes)
+        server_runner = ServerRunner.new(settings)
         _(server_runner.start).must_equal server_runner
       end
     end
