22199cc1e00bca483c9c51c4bc80878bc163fadb
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-05-20 21:11:01 +0200

    ProjectRepo: drop Settings dependency

--- lib/kamerling/project_repo.rb
+++ lib/kamerling/project_repo.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'client'
 require_relative 'project'
 require_relative 'repo'
-require_relative 'task'
 
 module Kamerling
   class ProjectRepo < Repo
-    def initialize(db = Settings.new.db_conn)
-      @klass = Project
-      @table = db[:projects]
+    private
+
+    def klass
+      Project
+    end
+
+    def table
+      db[:projects]
     end
   end
 end
