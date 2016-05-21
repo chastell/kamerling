b3a9ae643cd2c6b12e2d1b7ea65e8fdcd0969b66
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-05-21 10:55:29 +0200

    RegistrationRepo: drop Settings dependency

--- lib/kamerling/registration_repo.rb
+++ lib/kamerling/registration_repo.rb
@@ -2,13 +2,17 @@
 
 require_relative 'registration'
 require_relative 'repo'
-require_relative 'settings'
 
 module Kamerling
   class RegistrationRepo < Repo
-    def initialize(db = Settings.new.db_conn)
-      @klass = Registration
-      @table = db[:registrations]
+    private
+
+    def klass
+      Registration
+    end
+
+    def table
+      db[:registrations]
     end
   end
 end
