c23f102a8b39bfff83c9c03d9b1a54dc66f6ca05
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-05-22 08:46:26 +0200

    ResultRepo: drop Settings dependency

--- lib/kamerling/result_repo.rb
+++ lib/kamerling/result_repo.rb
@@ -2,13 +2,17 @@
 
 require_relative 'repo'
 require_relative 'result'
-require_relative 'settings'
 
 module Kamerling
   class ResultRepo < Repo
-    def initialize(db = Settings.new.db_conn)
-      @klass = Result
-      @table = db[:results]
+    private
+
+    def klass
+      Result
+    end
+
+    def table
+      db[:results]
     end
   end
 end
