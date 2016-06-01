05282d12ccd48f666a6ad9e0dd7a7c0c87a94c0b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-06-02 00:33:37 +0200

    move Settings::Repos to the main namespace

---
+++ lib/kamerling/repos.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'sequel'
+require_relative 'client_repo'
+require_relative 'dispatch_repo'
+require_relative 'project_repo'
+require_relative 'registration_repo'
+require_relative 'result_repo'
+require_relative 'task_repo'
+
+module Kamerling
+  class Repos
+    def initialize(db = 'sqlite::memory:')
+      @db_conn = Sequel.connect(db)
+    end
+
+    def client_repo
+      ClientRepo.new(db_conn)
+    end
+
+    def dispatch_repo
+      DispatchRepo.new(db_conn)
+    end
+
+    def project_repo
+      ProjectRepo.new(db_conn)
+    end
+
+    def registration_repo
+      RegistrationRepo.new(db_conn)
+    end
+
+    def result_repo
+      ResultRepo.new(db_conn)
+    end
+
+    def task_repo
+      TaskRepo.new(db_conn)
+    end
+
+    private
+
+    attr_reader :db_conn
+  end
+end
--- lib/kamerling/settings.rb
+++ lib/kamerling/settings.rb
@@ -2,14 +2,8 @@
 
 require 'forwardable'
 require 'optparse'
-require 'sequel'
 require_relative 'addr'
-require_relative 'client_repo'
-require_relative 'dispatch_repo'
-require_relative 'project_repo'
-require_relative 'registration_repo'
-require_relative 'result_repo'
-require_relative 'task_repo'
+require_relative 'repos'
 require_relative 'value'
 
 module Kamerling
@@ -58,40 +52,6 @@ module Kamerling
 
     def repos
       @repos ||= Repos.new(db)
-    end
-
-    class Repos
-      def initialize(db = 'sqlite::memory:')
-        @db_conn = Sequel.connect(db)
-      end
-
-      def client_repo
-        ClientRepo.new(db_conn)
-      end
-
-      def dispatch_repo
-        DispatchRepo.new(db_conn)
-      end
-
-      def project_repo
-        ProjectRepo.new(db_conn)
-      end
-
-      def registration_repo
-        RegistrationRepo.new(db_conn)
-      end
-
-      def result_repo
-        ResultRepo.new(db_conn)
-      end
-
-      def task_repo
-        TaskRepo.new(db_conn)
-      end
-
-      private
-
-      attr_reader :db_conn
     end
   end
 end
