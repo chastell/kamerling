352b6fc2a166e1f814d8eedae07427d3e8282934
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-06-16 00:23:10 +0200

    Settings: allow passing values to the constructor

--- lib/kamerling/settings.rb
+++ lib/kamerling/settings.rb
@@ -4,36 +4,39 @@ require 'dotenv'
 Dotenv.load
 
 require_relative 'addr'
+require_relative 'value'
 require_relative 'server/http'
 require_relative 'server/tcp'
 require_relative 'server/udp'
 
 module Kamerling
-  class Settings
+  class Settings < Value
+    vals host: String, http: Integer, tcp: Integer, udp: Integer
+
     def servers
       [
         Server::HTTP.new(addr: Addr[host, http, :TCP]),
         Server::TCP.new(addr:  Addr[host, tcp, :TCP]),
         Server::UDP.new(addr:  Addr[host, udp, :UDP]),
-      ].select { |server| server.addr.port }
+      ].select { |server| server.addr.host and server.addr.port }
     end
 
     private
 
     def host
-      ENV['HOST']
+      @host || ENV['HOST']
     end
 
     def http
-      ENV['HTTP']
+      @http || ENV['HTTP']
     end
 
     def tcp
-      ENV['TCP']
+      @tcp || ENV['TCP']
     end
 
     def udp
-      ENV['UDP']
+      @udp || ENV['UDP']
     end
   end
 end
