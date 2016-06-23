18756aa0829f7dd04ba6510d1b322fd352e4b968
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2016-06-23 06:31:43 +0200

    ServerRunner: add #default_servers

--- lib/kamerling/server_runner.rb
+++ lib/kamerling/server_runner.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'server/http'
+require_relative 'server/tcp'
+require_relative 'server/udp'
 require_relative 'settings'
 
 module Kamerling
   class ServerRunner
     def initialize(settings = Settings.new)
-      @servers = settings.servers
+      @servers = settings.servers || default_servers
     end
 
     def join
@@ -20,5 +23,11 @@ module Kamerling
     private
 
     attr_reader :servers
+
+    def default_servers
+      [Server::HTTP.new, Server::TCP.new, Server::UDP.new].select do |server|
+        server.addr.host and server.addr.port
+      end
+    end
   end
 end
