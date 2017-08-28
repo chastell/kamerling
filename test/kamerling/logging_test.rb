036fd17b0468ba3a8456910c044b45f58e119be0
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2017-08-29 00:08:56 +0200

    Switch Logging test to {TCP,UDP}Addr

--- test/kamerling/logging_test.rb
+++ test/kamerling/logging_test.rb
@@ -4,20 +4,21 @@ require 'logger'
 require 'socket'
 require 'stringio'
 require_relative '../test_helper'
-require_relative '../../lib/kamerling/addr'
 require_relative '../../lib/kamerling/logging'
 require_relative '../../lib/kamerling/net_dispatcher'
 require_relative '../../lib/kamerling/server/tcp'
 require_relative '../../lib/kamerling/server/udp'
+require_relative '../../lib/kamerling/tcp_addr'
+require_relative '../../lib/kamerling/udp_addr'
 
 module Kamerling
   describe Logging do
-    let(:logged)     { stream.tap(&:rewind).read                            }
-    let(:logger)     { Logger.new(stream)                                   }
-    let(:stream)     { StringIO.new                                         }
-    let(:tcp_server) { Server::TCP.new(addr: Addr['localhost', 1981, :TCP]) }
-    let(:udp_server) { Server::UDP.new(addr: Addr['localhost', 1979, :UDP]) }
-    let(:udp_client) { UDPSocket.new                                        }
+    let(:logged)     { stream.tap(&:rewind).read                         }
+    let(:logger)     { Logger.new(stream)                                }
+    let(:stream)     { StringIO.new                                      }
+    let(:tcp_server) { Server::TCP.new(addr: TCPAddr['localhost', 1981]) }
+    let(:udp_server) { Server::UDP.new(addr: UDPAddr['localhost', 1979]) }
+    let(:udp_client) { UDPSocket.new                                     }
 
     before do
       Logging.log_to logger
@@ -42,7 +43,7 @@ module Kamerling
 
       it 'logs TCP server connects' do
         tcp_addr = TCPSocket.open(*tcp_server.addr) do |socket|
-          Addr[*socket.local_address.ip_unpack, :TCP]
+          TCPAddr[*socket.local_address.ip_unpack]
         end
         run_all_threads
         _(logged).must_include "connect #{tcp_addr}"
@@ -51,7 +52,7 @@ module Kamerling
       it 'logs TCP server receives' do
         tcp_addr = TCPSocket.open(*tcp_server.addr) do |socket|
           socket << 'PING'
-          Addr[*socket.local_address.ip_unpack, :TCP]
+          TCPAddr[*socket.local_address.ip_unpack]
         end
         run_all_threads
         _(logged).must_include "received #{tcp_addr} 50 49 4e 47"
@@ -60,7 +61,7 @@ module Kamerling
       it 'logs TCP unknown message types' do
         tcp_addr = TCPSocket.open(*tcp_server.addr) do |socket|
           socket << 'foo'
-          Addr[*socket.local_address.ip_unpack, :TCP]
+          TCPAddr[*socket.local_address.ip_unpack]
         end
         run_all_threads
         _(logged).must_include "received #{tcp_addr} unknown message type"
@@ -77,7 +78,7 @@ module Kamerling
 
       it 'logs UDP server connects' do
         100.times { udp_client.send 'PING', 0, *udp_server.addr }
-        udp_addr = Addr['127.0.0.1', udp_client.addr[1], :UDP]
+        udp_addr = UDPAddr['127.0.0.1', udp_client.addr[1]]
         run_all_threads
         _(logged).must_include "connect #{udp_addr}"
       end
@@ -85,21 +86,21 @@ module Kamerling
       it 'logs UDP server receives' do
         run_all_threads
         100.times { udp_client.send 'PING', 0, *udp_server.addr }
-        udp_addr = Addr['127.0.0.1', udp_client.addr[1], :UDP]
+        udp_addr = UDPAddr['127.0.0.1', udp_client.addr[1]]
         run_all_threads
         _(logged).must_include "received #{udp_addr} 50 49 4e 47"
       end
 
       it 'logs UDP unknown message types' do
         100.times { udp_client.send 'foo', 0, *udp_server.addr }
-        udp_addr = Addr['127.0.0.1', udp_client.addr[1], :UDP]
+        udp_addr = UDPAddr['127.0.0.1', udp_client.addr[1]]
         run_all_threads
         _(logged).must_include "received #{udp_addr} unknown message type"
       end
 
       it 'logs packet dispatches' do
         server = UDPSocket.new.tap { |s| s.bind '127.0.0.1', 0 }
-        addr   = Addr[server.addr[3], server.addr[1], :UDP]
+        addr   = UDPAddr[server.addr[3], server.addr[1]]
         NetDispatcher.dispatch 'PING', addr: addr
         _(logged).must_include "sent #{addr} 50 49 4e 47"
       end
