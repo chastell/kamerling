373931edd7a12986b5b27df8485839065d77f049
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2017-08-30 00:18:26 +0200

    Switch NetDispatcher test to {TCP,UDP}Addr

--- test/kamerling/net_dispatcher_test.rb
+++ test/kamerling/net_dispatcher_test.rb
@@ -2,8 +2,9 @@
 
 require 'socket'
 require_relative '../test_helper'
-require_relative '../../lib/kamerling/addr'
 require_relative '../../lib/kamerling/net_dispatcher'
+require_relative '../../lib/kamerling/tcp_addr'
+require_relative '../../lib/kamerling/udp_addr'
 
 module Kamerling
   describe NetDispatcher do
@@ -11,7 +12,7 @@ module Kamerling
       it 'dispatches messages to TCP clients' do
         server = TCPServer.open(0)
         thread = Thread.new { server.accept.read }
-        addr   = Addr[server.addr[3], server.addr[1], :TCP]
+        addr   = TCPAddr[server.addr[3], server.addr[1]]
         NetDispatcher.dispatch 'PING', addr: addr
         _(thread.value).must_equal 'PING'
       end
@@ -19,7 +20,7 @@ module Kamerling
       it 'dispatches messages to UDP clients' do
         server = UDPSocket.new.tap { |s| s.bind '127.0.0.1', 0 }
         thread = Thread.new { server.recvfrom(2**16).first }
-        addr   = Addr[server.addr[3], server.addr[1], :UDP]
+        addr   = UDPAddr[server.addr[3], server.addr[1]]
         NetDispatcher.dispatch 'PING', addr: addr
         _(thread.value).must_equal 'PING'
       end
