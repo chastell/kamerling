502acae8bf3a5011cd4feb4779ed8d5d729e0fb0
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2017-09-04 00:06:27 +0200

    Switch Client test to UDPAddr

--- test/kamerling/client_test.rb
+++ test/kamerling/client_test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
-require_relative '../../lib/kamerling/addr'
 require_relative '../../lib/kamerling/client'
+require_relative '../../lib/kamerling/udp_addr'
 
 module Kamerling
   describe Client do
@@ -14,7 +14,7 @@ module Kamerling
 
     describe '#to_h' do
       it 'returns a Hash representation of the Client' do
-        addr   = Addr['127.0.0.1', 1979, :UDP]
+        addr   = UDPAddr['127.0.0.1', 1979]
         client = Client.new(addr: addr, busy: true, type: :FPGA)
         _(client.to_h).must_equal busy: true, host: '127.0.0.1',
                                   id: any(String), port: 1979, prot: 'UDP',
