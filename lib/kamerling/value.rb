3d0c200401fa3d4eca30c5fa3599d84c214ea176
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2018-02-22 11:36:45 +0100

    Switch Value to class << self

--- lib/kamerling/value.rb
+++ lib/kamerling/value.rb
@@ -2,17 +2,19 @@ require 'virtus'
 
 module Kamerling
   class Value
-    include Virtus.value_object
+    class << self
+      def defaults(hash = {})
+        hash.each do |name, default|
+          attribute name, attribute_set[name].type, default: default
+        end
+      end
 
-    def self.defaults(hash = {})
-      hash.each do |name, default|
-        attribute name, attribute_set[name].type, default: default
+      def vals(hash = {})
+        values { hash.each { |name, klass| attribute name, klass } }
       end
     end
 
-    def self.vals(hash = {})
-      values { hash.each { |name, klass| attribute name, klass } }
-    end
+    include Virtus.value_object
 
     # :reek:FeatureEnvy
     def to_h
